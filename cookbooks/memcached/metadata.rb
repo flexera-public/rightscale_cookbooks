c072a17975faf4e1482e1837f798ad60a91d38aa
Maksim Rusan <maksim.rusan@gmail.com>
2012-05-07 19:30:23 +0300

    rs_utils to rightscale

--- cookbooks/memcached/metadata.rb
+++ cookbooks/memcached/metadata.rb
@@ -6,7 +6,7 @@ long_description IO.read(File.join(File.dirname(__FILE__), 'README.rdoc'))
 version          "0.1.0"
 
 
-depends "rs_utils"
+depends "rightscale"
 depends "sys_firewall"
 depends "logrotate"
 
