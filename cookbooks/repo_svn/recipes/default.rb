492429ff3065a2072628079a1858cd5cb4887338
Neve <n.stakanov@tut.by>
2012-05-17 20:46:55 +0300

    Indentation fix

--- cookbooks/repo_svn/recipes/default.rb
+++ cookbooks/repo_svn/recipes/default.rb
@@ -7,26 +7,26 @@
 
 rightscale_marker :begin
 
-  # install subversion client
-  package "subversion" do
+# install subversion client
+package "subversion" do
+  action :install
+end
+
+extra_packages = case node[:platform]
+  when "ubuntu","debian"
+    if node[:platform_version].to_f < 8.04
+      %w{subversion-tools libsvn-core-perl}
+    else
+      %w{subversion-tools libsvn-perl}
+    end
+  when "centos","redhat","fedora"
+    %w{subversion-devel subversion-perl}
+end
+
+extra_packages.each do |pkg|
+  package pkg do
     action :install
   end
-
-  extra_packages = case node[:platform]
-    when "ubuntu","debian"
-      if node[:platform_version].to_f < 8.04
-        %w{subversion-tools libsvn-core-perl}
-      else
-        %w{subversion-tools libsvn-perl}
-      end
-    when "centos","redhat","fedora"
-      %w{subversion-devel subversion-perl}
-  end
-
-  extra_packages.each do |pkg|
-    package pkg do
-      action :install
-    end
-  end
+end
 
 rightscale_marker :end
