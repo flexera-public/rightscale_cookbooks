fcd62e3c8fe22e5cb1c716bad517cdaca2e58b36
Lopaka Delp <lopaka@rightscale.com>
2012-05-16 23:59:20 -0700

    [app_passenger::default] removed app resource call from default recipe.

--- cookbooks/app_passenger/recipes/default.rb
+++ cookbooks/app_passenger/recipes/default.rb
@@ -14,12 +14,6 @@ node[:app][:database_name] = node[:app_passenger][:project][:db][:schema_name]
 node[:app][:root] = "#{node[:app][:destination]}/public"
 node[:app][:port] = 8000
 
-app "default" do
-  provider node[:app][:provider]
-  persist true
-  action :nothing
-end
-
 case node[:platform]
   when "ubuntu","debian"
     node[:app][:packages] = ["libopenssl-ruby", "libcurl4-openssl-dev", "apache2-mpm-prefork", "apache2-prefork-dev", "libapr1-dev", "libcurl4-openssl-dev"]
