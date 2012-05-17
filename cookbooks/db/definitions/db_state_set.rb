7eaa49566efb0723b705761316a25a3530c012a1
Douglas Thrift <douglas.thrift@rightscale.com>
2012-05-17 00:30:59 +0000

    [db] Add a db_state_set definition to set the master/slave state in both a JSON file and in the node.

---
+++ cookbooks/db/definitions/db_state_set.rb
@@ -0,0 +1,47 @@
+#
+# Cookbook Name:: db
+#
+# Copyright RightScale, Inc. All rights reserved.  All access and use subject to the
+# RightScale Terms of Service available at http://www.rightscale.com/terms.php and,
+# if applicable, other agreements such as a RightScale Master Subscription Agreement.
+
+# Set or reset the master/slave status, UUID, and IP.
+#
+# @param [String, nil] master_uuid the UUID of the master server to set
+# @param [String, nil] master_ip the IP address of the master server to set
+# @param [Boolean] is_master set whether this server is the master
+# @param [Boolean] immediate run the resource actions immediately
+#
+define :db_state_set, :master_uuid => nil, :master_ip => nil, :is_master => false, :immediate => false do
+  name = params[:name]
+  master_uuid = params[:master_uuid]
+  master_ip = params[:master_ip]
+  is_master = params[:is_master]
+  immediate = params[:immediate]
+
+  r = log "#{name}: master UUID: #{master_uuid} IP: #{master} this is #{is_master ? "master" : "slave"}" do
+    action immediate ? :nothing : :write
+  end
+  r.run_action(:write) if immediate
+
+  r = ruby_block "#{name}: set in node" do
+    block do
+      node[:db][:current_master_uuid] = master_uuid
+      node[:db][:current_master_ip] = master_ip
+      node[:db][:this_is_master] = is_master
+    end
+    action immediate ? :nothing : :create
+  end
+  r.run_action(:create) if immediate
+
+  r = file "/var/lib/rightscale_db_master_slave_state" do
+    backup false
+    content JSON.dump({
+      "master_uuid" => master_uuid,
+      "master_ip" => master_ip,
+      "is_master" => is_master
+    })
+    action immediate ? :nothing : :create
+  end
+  r.run_action(:create) if immediate
+end
