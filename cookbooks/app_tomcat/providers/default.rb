flexera-public/cookbooks_public

[app_tomcat]typo

cookbooks/app_tomcat/providers/default.rb
@@ -147,7 +147,7 @@ action :setup_vhost do
     mode "0644"
     cookbook 'app_tomcat'
     variables(
-            :doc_root => node[:tomcat][:docroot]
+            :doc_root => node[:tomcat][:docroot],
             :app_port => node[:app][:port]
           )
   end

flexera-public/rightscale_cookbooks

cookbooks/app_tomcat/providers/default.rb
@@ -164,7 +164,7 @@ action :setup_vhost do
     mode "0644"
     cookbook 'app_tomcat'
     variables(
-            :doc_root => node[:app_tomcat][:docroot]
+            :doc_root => node[:app_tomcat][:docroot],
             :app_port => node[:app][:port]
           )
   end