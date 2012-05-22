cookbooks/rs-sys

[sys] Since we are not supporting Chef 0.8, just remove that test for just 0.9.

libraries/helper.rb
@@ -48,11 +48,7 @@ module RightScale
         resrc.backup false
         #resrc.notifies notify_action, notify_resources
         
-        if (Chef::VERSION =~ /0\.9/) # provider signature changed in Chef 0.9
-          provider = Chef::Provider::Template.new(resrc, run_context)
-        else 
-          provider = Chef::Provider::Template.new(node, resrc)
-        end
+        provider = Chef::Provider::Template.new(resrc, run_context)
         provider.load_current_resource
                
         if enable

flexera-public/rightscale_cookbooks

cookbooks/sys/libraries/helper.rb
@@ -30,11 +30,7 @@ module RightScale
         resrc = Chef::Resource::ServerCollection.new(collection_name)
         resrc.tags tag
         provider = nil      
-        if (Chef::VERSION =~ /0\.9/) # provider signature changed in Chef 0.9
-          provider = Chef::Provider::ServerCollection.new(resrc, run_context)
-        else 
-          provider = Chef::Provider::ServerCollection.new(node, resrc)
-        end
+        provider = Chef::Provider::ServerCollection.new(resrc, run_context)
         provider.send("action_load")
       end
             