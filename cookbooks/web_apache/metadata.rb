99fa133dace350cad746a225b12b7145bca14e12
Erik Kargard <erik@rightscale.com>
2012-05-22 20:21:16 +0000

    [web_apache]removed application_name dependency for default. w4586 (and fixed formatting)

--- cookbooks/web_apache/metadata.rb
+++ cookbooks/web_apache/metadata.rb
@@ -17,17 +17,17 @@ recipe "web_apache::setup_monitoring", "Install collectd-apache for monitoring s
 
 
 all_recipes = [
-                "web_apache::default",
-                "web_apache::install_apache",
-                "web_apache::setup_frontend_ssl_vhost",
-                "web_apache::setup_frontend_http_vhost",
-                "web_apache::setup_frontend",
-              ]
+  "web_apache::default",
+  "web_apache::install_apache",
+  "web_apache::setup_frontend_ssl_vhost",
+  "web_apache::setup_frontend_http_vhost",
+  "web_apache::setup_frontend",
+]
 other_recipes = [
-                "web_apache::do_start",
-                "web_apache::do_stop",
-                "web_apache::do_restart"
-              ]
+  "web_apache::do_start",
+  "web_apache::do_stop",
+  "web_apache::do_restart"
+]
 
 depends "apache2"
 depends "rightscale"
@@ -49,10 +49,10 @@ attribute "web_apache/ssl_enable",
   :display_name => "SSL Enable",
   :description => "Enables SSL ('https')",
   :recipes => [
-                "web_apache::install_apache",
-                "web_apache::setup_frontend_ssl_vhost",
-                "web_apache::setup_frontend"
-              ],
+    "web_apache::install_apache",
+    "web_apache::setup_frontend_ssl_vhost",
+    "web_apache::setup_frontend"
+   ],
   :required => "optional",
   :choice => [ "true", "false" ],
   :default =>  "false"
@@ -62,32 +62,48 @@ attribute "web_apache/ssl_certificate",
   :description => "The name of your SSL Certificate.",
   :required => "optional",
   :default =>  "",
-  :recipes => [ "web_apache::setup_frontend_ssl_vhost","web_apache::setup_frontend" ]
+  :recipes => [
+    "web_apache::setup_frontend_ssl_vhost",
+    "web_apache::setup_frontend" 
+  ]
 
 attribute "web_apache/ssl_certificate_chain",
   :display_name => "SSL Certificate Chain",
   :description => "Your SSL Certificate Chain.",
   :required => "optional",
   :default =>  "",
-  :recipes => [ "web_apache::setup_frontend_ssl_vhost","web_apache::setup_frontend" ]
+  :recipes => [
+    "web_apache::setup_frontend_ssl_vhost",
+    "web_apache::setup_frontend" 
+  ]
 
 attribute "web_apache/ssl_key",
   :display_name => "SSL Certificate Key",
   :description => "Your SSL Certificate Key.",
   :required => "optional",
   :default =>  "",
-  :recipes => [ "web_apache::setup_frontend_ssl_vhost","web_apache::setup_frontend" ]
+  :recipes => [
+    "web_apache::setup_frontend_ssl_vhost",
+    "web_apache::setup_frontend" 
+  ]
 
 attribute "web_apache/ssl_passphrase",
   :display_name => "SSL passphrase",
   :description => "Your SSL passphrase.",
   :required => "optional",
   :default =>  "",
-  :recipes => [ "web_apache::setup_frontend_ssl_vhost","web_apache::setup_frontend" ]
+  :recipes => [
+    "web_apache::setup_frontend_ssl_vhost",
+    "web_apache::setup_frontend" 
+  ]
 
 attribute "web_apache/application_name",
   :display_name => "Application Name",
   :description => "Sets the directory for your application's web files (/home/webapps/Application Name/current/).  If you have multiple applications, you can run the code checkout script multiple times, each with a different value for APPLICATION, so each application will be stored in a unique directory.  This must be a valid directory name.  Do not use symbols in the name.",
   :required => "optional",
   :default => "myapp",
-  :recipes => all_recipes
+  :recipes => [
+    "web_apache::setup_frontend_ssl_vhost",
+    "web_apache::setup_frontend_http_vhost",
+    "web_apache::setup_frontend",
+  ]
