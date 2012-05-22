793f91dd09efdeb8a7b5b52bd258476a66fcf481
Erik Kargard <erik@rightscale.com>
2012-05-22 17:10:45 +0000

    [app_*]Cleanup

--- cookbooks/app_php/recipes/default.rb
+++ cookbooks/app_php/recipes/default.rb
@@ -8,23 +8,42 @@
 rightscale_marker :begin
 
 log "  Setting provider specific settings for php application server."
-
 node[:app][:provider] = "app_php"
 
 case node[:platform]
 when "ubuntu", "debian"
   if node[:app_php][:db_adapter] == "mysql"
-    node[:app][:packages] = ["php5", "php5-mysql", "php-pear", "libapache2-mod-php5"]
+    node[:app][:packages] = [
+      "php5",
+      "php5-mysql",
+      "php-pear",
+      "libapache2-mod-php5"
+       ]
   elsif node[:app_php][:db_adapter] == "postgresql"
-    node[:app][:packages] = ["php5", "php5-pgsql", "php-pear", "libapache2-mod-php5"]
+    node[:app][:packages] = [
+      "php5",
+      "php5-pgsql",
+      "php-pear",
+      "libapache2-mod-php5"
+      ]
   else
     raise "Unrecognized database adapter #{node[:app][:db_adapter]}, exiting "
   end
 when "centos","fedora","suse","redhat"
   if node[:app_php][:db_adapter] == "mysql"
-    node[:app][:packages] = ["php53u", "php53u-mysql", "php53u-pear", "php53u-zts"]
+    node[:app][:packages] = [
+      "php53u",
+      "php53u-mysql",
+      "php53u-pear",
+      "php53u-zts"
+      ]
   elsif node[:app_php][:db_adapter] == "postgresql"
-    node[:app][:packages] = ["php53u", "php53u-pgsql", "php53u-pear", "php53u-zts"]
+    node[:app][:packages] = [
+      "php53u",
+      "php53u-pgsql",
+      "php53u-pear",
+      "php53u-zts"
+      ]
   else
     raise "Unrecognized database adapter #{node[:app_php][:db_adapter]}, exiting "
   end
@@ -34,22 +53,18 @@ end
 
 
 log " Preparing php document root variable"
+dest_dir="/home/php/webapps"
 if node[:repo][:default][:destination].empty?
-  log "  Your repo/default/destination input is no set. Setting project root to default: /home/php/webapps/ "
-  node[:app_php][:project_home]= "/home/php/webapps/"
+  log "  Your repo/default/destination input is no set. Setting project root to default: #{dest_dir}"
+  project_home = dest_dir
 else
-  node[:app_php][:project_home]= node[:repo][:default][:destination]
+  project_home = node[:repo][:default][:destination]
 end
 
-# Creating new project root directory
-directory "#{node[:app_php][:project_home]}" do
-  recursive true
-end
 # Cooking doc root variable
-node[:app_php][:doc_root] = "#{node[:app_php][:project_home]}/#{node[:web_apache][:application_name]}"
+node[:app][:root] = "#{project_home]}/#{node[:web_apache][:application_name]}"
 
 # Setting app LWRP attribute
 node[:app][:destination]="#{node[:app_php][:doc_root]}"
-
 
 rightscale_marker :end
