9bf264203eb3d49d11018bd730e00502de0838b9
Neve <n.stakanov@tut.by>
2012-05-23 16:55:21 +0300

    attributes\apache.rb ->attributes\default.rb  more comments added

--- cookbooks/web_apache/attributes/default.rb
+++ cookbooks/web_apache/attributes/default.rb
@@ -5,23 +5,29 @@
 # RightScale Terms of Service available at http://www.rightscale.com/terms.php and,
 # if applicable, other agreements such as a RightScale Master Subscription Agreement.
 
-#
+
+#Changing values of "apache2" cookbook to adopt apache2 settings to RightScale needs
+
 # Recommended attributes
-#
 set_unless[:apache][:contact] = "root@localhost"
 
-#
+
 # Optional attributes
-#
+
 # Turning off Keepalive to prevent conflicting HAproxy
 set_unless[:apache][:keepalive] = "Off" 
 # Turn on generation of "full" apache status
 set_unless[:apache][:extended_status] = "On"
-#  worker = multithreaded
-#  prefork = single-threaded (use for php)
+#  worker = multithreaded (when you need a great deal of scalability)
+#  prefork = single-threaded (when you need stability or compatibility with older software)
+# for more info please visit: http://httpd.apache.org/docs/2.0/en/mpm.html
 set_unless[:apache][:mpm] = "prefork"
 # Security: Configuring Server Signature
 set_unless[:apache][:serversignature] = "Off "
+
+
+# Calculated attributes
+
 # DISTRO specific config dir
 case platform
 when "ubuntu", "debian"
@@ -30,6 +36,7 @@ when "centos", "fedora", "suse","redhat"
   set[:apache][:config_subdir] = "httpd"
 end
 
+# Disabling ssl by default
 set_unless[:web_apache][:ssl_enable] = false
 set_unless[:web_apache][:ssl_certificate] = nil
 set_unless[:web_apache][:ssl_certificate_chain] = nil
@@ -38,4 +45,6 @@ set_unless[:web_apache][:ssl_passphrase] = nil
 
 # Used to be called php/code/destination
 set[:web_apache][:docroot] = "/home/webapp/#{web_apache[:application_name]}"
+
+# Default servername for web_apache vhost file
 set[:web_apache][:server_name] = "localhost"
