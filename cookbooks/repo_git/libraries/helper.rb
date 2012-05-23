86bb831959f3e24af550a64e0139e53e058da844
Neve <n.stakanov@tut.by>
2012-05-23 11:38:19 +0300

    more comments to repo_git  cookbook.

--- cookbooks/repo_git/libraries/helper.rb
+++ cookbooks/repo_git/libraries/helper.rb
@@ -5,33 +5,45 @@
 # RightScale Terms of Service available at http://www.rightscale.com/terms.php and,
 # if applicable, other agreements such as a RightScale Master Subscription Agreement.
 
+
+
 module RightScale
   module Repo
     class Ssh_key
      KEYFILE = "/tmp/gitkey"
 
-     # Add ssh key and exec script
+     #  Create bash script, which will set user defined ssh key required to access to private git source code repositories.
+     #  Parameters:
+     #    ssh_key(String):: Git private ssh key
+     #  Raise:
+     #    RuntimeError:: if ssh key string is empty
      def create(git_ssh_key)
-       Chef::Log.info("Creating ssh key")
-
+       Chef::Log.info("  Creating ssh key")
        keyfile = nil
        keyname = git_ssh_key
+
        if "#{keyname}" != ""
          keyfile = KEYFILE
+         # Writing key to file
          system("echo -n '#{keyname}' > #{keyfile}")
+         # Setting permissions
          system("chmod 700 #{keyfile}")
+         # Adding additional parameters
          system("echo 'exec ssh -oStrictHostKeyChecking=no -i #{keyfile} \"$@\"' > #{keyfile}.sh")
          system("chmod +x #{keyfile}.sh")
        end
-
+       # GIT_SSH environment variable.
        ENV["GIT_SSH"] = "#{keyfile}.sh" unless ("#{keyfile}" == "")
      end
 
-     # Delete SSH key & clear GIT_SSH
+     #  Delete SSH key created by "create" method, after successful pull operation. And clear GIT_SSH.
+     #  Parameters:
+     #    none
      def delete
        Chef::Log.warn "Deleting ssh key "
         keyfile = KEYFILE
        if keyfile != nil
+         # Removing previously created files.
          system("rm -f #{keyfile}")
          system("rm -f #{keyfile}.sh")
        end
