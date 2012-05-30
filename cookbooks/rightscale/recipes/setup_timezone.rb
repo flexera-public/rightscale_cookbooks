5c3e1f36a94b51a896644bfbf4c9e61c41d6f8cc
Erik Kargard <erik@rightscale.com>
2012-05-30 21:24:10 +0000

    [rightscale] Updated sys timezone log messages

--- cookbooks/rightscale/recipes/setup_timezone.rb
+++ cookbooks/rightscale/recipes/setup_timezone.rb
@@ -5,24 +5,20 @@
 # RightScale Terms of Service available at http://www.rightscale.com/terms.php and,
 # if applicable, other agreements such as a RightScale Master Subscription Agreement.
 
+rightscale_marker :begin
+
 # == Set the Timezone
 #
 if node[:rightscale][:timezone]
-  
-  rightscale_marker :begin
-
   link "/etc/localtime" do
     to "/usr/share/zoneinfo/#{node[:rightscale][:timezone]}"
   end
-
-  log "Timezone set to #{node[:rightscale][:timezone]}"
-
+  log "  Timezone set to #{node[:rightscale][:timezone]}"
 else 
 
   # If this attribute is not set leave unchanged and use localtime
-  log "rightscale/timezone set to localtime.  Not changing /etc/localtime..."
-  
-  rightscale_marker :end
-  
+  log "  rightscale/timezone set to localtime.  Not changing /etc/localtime..."
 end
+
+rightscale_marker :end
 
