46ebb6f882f4155398482ff8b7eba9bc971600d0
Neve <n.stakanov@tut.by>
2012-06-04 15:38:28 +0300

    [db_postgres] comments fixes to definitions

--- cookbooks/db_postgres/definitions/db_postgres_set_privileges.rb
+++ cookbooks/db_postgres/definitions/db_postgres_set_privileges.rb
@@ -19,10 +19,10 @@ define :db_postgres_set_privileges, :preset => "administrator", :username => nil
       require 'rubygems'
       Gem.clear_paths
       require 'pg'
-	    sleep 20
-	    conn = PGconn.open("localhost", nil, nil, nil, nil, "postgres", nil)
+      sleep 20
+      conn = PGconn.open("localhost", nil, nil, nil, nil, "postgres", nil)
 
-      # Now that we have a Postgresql object, let's santize our inputs
+      # Now that we have a Postgresql object, let's sanitize our inputs
       username = conn.escape_string(username)
       password = conn.escape_string(password)
 
@@ -32,14 +32,14 @@ define :db_postgres_set_privileges, :preset => "administrator", :username => nil
       # from group role, see: http://www.postgresql.org/docs/9.1/static/role-membership.html 
       # cmd ==> createuser -h /var/run/postgresql -U postgres #{admin_role} -sdril 
       
-      # Enable admin/replication user
+        # Enable admin/replication user
         result = conn.exec("SELECT COUNT(*) FROM pg_user WHERE usename='#{username}'")
         userstat = result.getvalue(0,0)
         if ( userstat == '1' )
-          Chef::Log.info "User #{username} already exists, updating user using current inputs"
+          Chef::Log.info "  User #{username} already exists, updating user using current inputs"
           conn.exec("ALTER USER #{username} SUPERUSER CREATEDB CREATEROLE INHERIT LOGIN ENCRYPTED PASSWORD '#{password}'")
         else
-          Chef::Log.info "creating administrator user #{username}"
+          Chef::Log.info "  Creating administrator user #{username}"
           conn.exec("CREATE USER #{username} SUPERUSER CREATEDB CREATEROLE INHERIT LOGIN ENCRYPTED PASSWORD '#{password}'")
         end
 
@@ -48,18 +48,18 @@ define :db_postgres_set_privileges, :preset => "administrator", :username => nil
       # from group role, see: http://www.postgresql.org/docs/9.1/static/role-membership.html
       # cmd ==> createuser -h /var/run/postgresql -U postgres #{user_role} -SdRil 
       
-      # Enable application user  
+        # Enable application user
         result = conn.exec("SELECT COUNT(*) FROM pg_user WHERE usename='#{username}'")
         userstat = result.getvalue(0,0)
         if ( userstat == '1' )
-          Chef::Log.info "User #{username} already exists, updating user using current inputs"
+          Chef::Log.info "  User #{username} already exists, updating user using current inputs"
           conn.exec("ALTER USER #{username} NOSUPERUSER CREATEDB NOCREATEROLE INHERIT LOGIN ENCRYPTED PASSWORD '#{password}'")
         else
-          Chef::Log.info "creating aplication user #{username}"
+          Chef::Log.info "  Creating application user #{username}"
           conn.exec("CREATE USER #{username} NOSUPERUSER CREATEDB NOCREATEROLE INHERIT LOGIN ENCRYPTED PASSWORD '#{password}'")
         end
 
-      # Set default privileges for any future tables, sequences, or functions created.
+        # Set default privileges for any future tables, sequences, or functions created.
         conn.exec("ALTER DEFAULT PRIVILEGES FOR USER #{username} GRANT ALL ON TABLES to #{username}")
         conn.exec("ALTER DEFAULT PRIVILEGES FOR USER #{username} GRANT ALL ON SEQUENCES to #{username}")
         conn.exec("ALTER DEFAULT PRIVILEGES FOR USER #{username} GRANT ALL ON FUNCTIONS to #{username}")
