eebc90e8f0dbad04c280f719f4c1a81255c71efe
Lopaka Delp <lopaka@rightscale.com>
2012-06-06 23:26:39 -0700

    [db_mysql] Create db_mysql/definitions/db_mysql_set_mycnf.rb, and consolidated 3 template resources with a definition.

---
+++ cookbooks/db_mysql/definitions/db_mysql_set_mycnf.rb
@@ -0,0 +1,22 @@
+#
+# Cookbook Name:: db_mysql
+#
+# Copyright RightScale, Inc. All rights reserved.  All access and use subject to the
+# RightScale Terms of Service available at http://www.rightscale.com/terms.php and,
+# if applicable, other agreements such as a RightScale Master Subscription Agreement.
+
+define :db_mysql_set_mycnf, :server_id => nil, :relay_log => nil do
+
+  template value_for_platform([ "centos", "redhat", "suse" ] => {"default" => "/etc/my.cnf"}, "default" => "/etc/mysql/my.cnf") do
+    source "my.cnf.erb"
+    owner "root"
+    group "root"
+    mode "0644"
+    variables(
+      :server_id => params[:server_id],
+      :relay_log => params[:relay_log]
+    )
+    cookbook "db_mysql"
+  end
+
+end
