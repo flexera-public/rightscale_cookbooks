08898062f2cef84f00633f9cbe6cb9296b2f263a
Neve <n.stakanov@tut.by>
2012-06-12 13:53:27 +0300

    [app] add reload resource, recipe, and default action

---
+++ cookbooks/app/recipes/do_server_reload.rb
@@ -0,0 +1,18 @@
+#
+# Cookbook Name::app
+#
+# Copyright RightScale, Inc. All rights reserved.  All access and use subject to the
+# RightScale Terms of Service available at http://www.rightscale.com/terms.php and,
+# if applicable, other agreements such as a RightScale Master Subscription Agreement.
+
+# This recipe will call "restart" action of "app" LightWeight resource
+#  For more info please see "restart" action definition in connected LightWeight provider (ex. app_php/providers/default.rb)
+
+rightscale_marker :begin
+
+log "  Reloading application server now..."
+app "default" do
+  action :reload
+end
+
+rightscale_marker :end
