caee121a9010d2e8e15eef830ecf261a89c39ea8
Douglas Thrift <douglas.thrift@rightscale.com>
2012-06-20 22:22:50 +0000

    [lb] Code formatting fix.

--- cookbooks/lb/recipes/do_attach_all.rb
+++ cookbooks/lb/recipes/do_attach_all.rb
@@ -57,7 +57,7 @@ vhosts(node[:lb][:vhost_names]).each do |vhost_name|
 
   # Set threshold counters to nil to those not incremented, thus assuming app server now accessable.
   # Set to nil since chef does not delete the key, can only alter it.
-  (Set.new(node[:lb][:threshold][vhost_name].keys)-servers_missing).each do |uuid|
+  (Set.new(node[:lb][:threshold][vhost_name].keys) - servers_missing).each do |uuid|
     if node[:lb][:threshold][vhost_name][uuid]
       node[:lb][:threshold][vhost_name][uuid] = nil
       log "  Resetting threshold for #{uuid}"
