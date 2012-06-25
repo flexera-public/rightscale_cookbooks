dd5c049f8fdb7b5c1a95a4af9a4649b5f6085e48
Lopaka Delp <lopaka@rightscale.com>
2012-06-25 16:39:01 -0700

    [block_device],[db] - updated metadata description of backup/timestamp_override stating this is only used in AWS

--- cookbooks/db/metadata.rb
+++ cookbooks/db/metadata.rb
@@ -200,7 +200,7 @@ attribute "db/backup/lineage_override",
 
 attribute "db/backup/timestamp_override",
   :display_name => "Database Restore Timestamp Override",
-  :description => "An optional variable to restore a database backup with a specific timestamp rather than the most recent backup in the lineage. You must specify a string that matches the timestamp tag on the volume snapshot. You will need to specify the timestamp that is defined by the snapshot's tag (not the name). For example, if the snapshot's tag is 'rs_backup:timestamp=1303613371' you would specify '1303613371' for this input.",
+  :description => "An optional variable to restore a database backup with a specific timestamp rather than the most recent backup in the lineage. You must specify a string that matches the timestamp tag on the volume snapshot. You will need to specify the timestamp that is defined by the snapshot's tag (not the name). This is only used with AWS. For example, if the snapshot's tag is 'rs_backup:timestamp=1303613371' you would specify '1303613371' for this input.",
   :required => "optional",
   :recipes => [
     "db::do_primary_restore_and_become_master",
