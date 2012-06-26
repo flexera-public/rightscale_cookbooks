875f6412f63252286cedb676079b105683741edf
mrusan <maksim.rusan@rightscale.com>
2012-06-26 12:13:31 +0300

    [repo_ftp repo_rsync] started dev of cookbooks

---
+++ cookbooks/repo_rsync/recipes/default.rb
@@ -0,0 +1,13 @@
+#
+# Cookbook Name:: repo_ros
+#
+#
+# Copyright RightScale, Inc. All rights reserved.  All access and use subject to the
+# RightScale Terms of Service available at http://www.rightscale.com/terms.php and,
+# if applicable, other agreements such as a RightScale Master Subscription Agreement.
+
+rightscale_marker :begin
+
+raise "  ROS gem missing, please add rightscale::install_tools recipes to runlist." unless File.exists?("/opt/rightscale/sandbox/bin/ros_util")
+
+rightscale_marker :end
