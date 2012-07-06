012eec9dfa61da96e07169c5d57dff3aac44fb73
Lopaka Delp <lopaka@rightscale.com>
2012-07-05 17:35:52 -0700

    [db_mysql::default_5_5] on centos 6.2, will remove mysql-libs which will remove postfix and installs postfix after mysql55-libs are installed

--- cookbooks/db_mysql/recipes/default_5_5.rb
+++ cookbooks/db_mysql/recipes/default_5_5.rb
@@ -17,14 +17,19 @@ node[:db_mysql][:version] = version
 platform = node[:platform]
 case platform
 when "redhat","centos","fedora","suse"
-# http://dev.mysql.com/doc/refman/5.5/en/linux-installation-native.html
-# For Red Hat and similar distributions, the MySQL distribution is divided into a 
-# number of separate packages, mysql for the client tools, mysql-server for the 
-# server and associated tools, and mysql-libs for the libraries. 
+  # http://dev.mysql.com/doc/refman/5.5/en/linux-installation-native.html
+  # For Red Hat and similar distributions, the MySQL distribution is divided into a 
+  # number of separate packages, mysql for the client tools, mysql-server for the 
+  # server and associated tools, and mysql-libs for the libraries. 
   node[:db_mysql][:service_name] = "mysqld"
-  node[:db_mysql][:packages_uninstall] = ""
-  node[:db_mysql][:client_packages_install] = ["mysql55-devel", "mysql55-libs", "mysql55"]
-  node[:db_mysql][:server_packages_install] = ["mysql55-server"]
+  node[:db_mysql][:packages_uninstall] = [ ]
+  node[:db_mysql][:client_packages_install] = [ "mysql55-devel", "mysql55-libs", "mysql55" ]
+  node[:db_mysql][:server_packages_install] = [ "mysql55-server" ]
+  # centos 6.2 by default has mysql-libs 5.1 installed as requirement for postfix
+  if platform == "centos" && node[:platform_version] == "6.2"
+    node[:db_mysql][:packages_uninstall] << "mysql-libs"
+    node[:db_mysql][:client_packages_install] << "postfix"
+  end
 else
   raise "Unsupported platform #{platform} for MySQL Version #{version}"
 end
