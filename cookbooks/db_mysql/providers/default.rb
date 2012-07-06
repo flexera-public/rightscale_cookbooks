7fed1df40047b53db6fc727b6172fa3eac777e19
Lopaka Delp <lopaka@rightscale.com>
2012-07-05 23:53:06 -0700

    cherry-picked 6bf5fd5efd85b1bba999cd9deeee650aa5d327fe from release13.00

--- cookbooks/db_mysql/providers/default.rb
+++ cookbooks/db_mysql/providers/default.rb
@@ -55,7 +55,7 @@ end
 action :firewall_update_request do
   sys_firewall "Sending request to open port 3306 (MySQL) allowing this server to connect" do
     machine_tag new_resource.machine_tag
-    port 3306 
+    port 3306
     enable new_resource.enable
     ip_addr new_resource.ip_addr
     action :update_request
@@ -65,7 +65,7 @@ end
 action :firewall_update do
   sys_firewall "Opening port 3306 (MySQL) for tagged '#{new_resource.machine_tag}' to connect" do
     machine_tag new_resource.machine_tag
-    port 3306 
+    port 3306
     enable new_resource.enable
     action :update
   end
@@ -112,7 +112,7 @@ action :post_restore_cleanup do
     block do
       master_info = RightScale::Database::MySQL::Helper.load_replication_info(node)
       # Check version matches
-      # Not all 11H2 snapshots (prior to 5.5 release) saved provider or version.  
+      # Not all 11H2 snapshots (prior to 5.5 release) saved provider or version.
       # Assume MySQL 5.1 if nil
       snap_version=master_info['DB_Version']||='5.1'
       snap_provider=master_info['DB_Provider']||='db_mysql'
@@ -162,10 +162,11 @@ action :install_client do
   packages = node[:db_mysql][:client_packages_uninstall]
   log "  Packages to uninstall: #{packages.join(",")}" unless packages == ""
   packages.each do |p|
-     package p do
-       action :remove
-     end
-  end unless packages == ""
+    r = package p do
+      action :nothing
+    end
+    r.run_action(:remove)
+  end
 
   # Install MySQL client packages
   # Must install during the compile stage because mysql gem build depends on the libs
@@ -303,7 +304,7 @@ action :install_server do
   # Timeouts enabled.
   # Ubuntu's init script does not support configurable startup timeout
   #
-  log_msg = ( platform =~ /redhat|centos/ ) ?  "  Setting mysql startup timeout" : "  Skipping mysql startup timeout setting for Ubuntu" 
+  log_msg = ( platform =~ /redhat|centos/ ) ?  "  Setting mysql startup timeout" : "  Skipping mysql startup timeout setting for Ubuntu"
   log log_msg
   template "/etc/sysconfig/#{node[:db_mysql][:service_name]}" do
     source "sysconfig-mysqld.erb"
@@ -436,7 +437,7 @@ action :promote do
     server_id RightScale::Database::MySQL::Helper.mycnf_uuid(node)
     relay_log RightScale::Database::MySQL::Helper.mycnf_relay_log(node)
   end
-  
+
   db node[:db][:data_dir] do
     action :start
     persist false
@@ -601,7 +602,7 @@ action :enable_replication do
       master_info = RightScale::Database::MySQL::Helper.load_replication_info(node)
       newmaster_host = master_info['Master_IP']
       newmaster_logfile = master_info['File']
-      newmaster_position = master_info['Position'] 
+      newmaster_position = master_info['Position']
       RightScale::Database::MySQL::Helper.reconfigure_replication(node, 'localhost', newmaster_host, newmaster_logfile, newmaster_position)
     end
   end
@@ -613,7 +614,7 @@ action :enable_replication do
       master_info = RightScale::Database::MySQL::Helper.load_master_info_file(node)
       newmaster_host = node[:db][:current_master_ip]
       newmaster_logfile = master_info['File']
-      newmaster_position = master_info['Position'] 
+      newmaster_position = master_info['Position']
       RightScale::Database::MySQL::Helper.reconfigure_replication(node, 'localhost', newmaster_host, newmaster_logfile, newmaster_position)
     end
   end
@@ -641,7 +642,7 @@ action :generate_dump_file do
 end
 
 action :restore_from_dump_file do
- 
+
   db_name   = new_resource.db_name
   dumpfile  = new_resource.dumpfile
   db_check  = `mysql -e "SHOW DATABASES LIKE '#{db_name}'"`
@@ -654,18 +655,18 @@ action :restore_from_dump_file do
       end
     end
   end
-  
+
   bash "Import MySQL dump file: #{dumpfile}" do
     only_if { db_check.empty? }
     user "root"
     flags "-ex"
     code <<-EOH
-      if [ ! -f #{dumpfile} ] 
-      then 
-        echo "ERROR: MySQL dumpfile not found! File: '#{dumpfile}'" 
+      if [ ! -f #{dumpfile} ]
+      then
+        echo "ERROR: MySQL dumpfile not found! File: '#{dumpfile}'"
         exit 1
-      fi 
-      mysqladmin -u root create #{db_name} 
+      fi
+      mysqladmin -u root create #{db_name}
       gunzip < #{dumpfile} | mysql -u root -b #{db_name}
     EOH
   end
