884a2333a5d3357f6c6daa0db6a58f30034f5236
Anand Ubale <anand@gmail.com>
2012-07-06 11:29:19 +0530

    pg bugfixes

--- cookbooks/app_tomcat/providers/default.rb
+++ cookbooks/app_tomcat/providers/default.rb
@@ -78,7 +78,7 @@ action :install do
     # Copy to /usr/share/java/postgresql-9.1-901.jdbc4.jar
     cookbook_file "/usr/share/java/postgresql-9.1-901.jdbc4.jar" do
       source "postgresql-9.1-901.jdbc4.jar"
-      owner "root"
+      owner node[:app_tomcat][:app_user]
       group "root"
       cookbook 'app_tomcat'
     end
@@ -320,17 +320,28 @@ action :setup_db_connection do
 
   db_name = new_resource.database_name
   db_adapter = node[:app_tomcat][:db_adapter]
-  datasource = node[:app_tomcat][:datasource_name]
 
-  log "  Creating context.xml for DB: #{db_name} using adapter #{db_adapter} and datasource #{datasource}"
-  db_connect_app "/etc/tomcat6/context.xml" do
-    template      "context_xml.erb"
-    owner         "#{node[:app_tomcat][:user]}"
-    group         "root"
-    mode          "0644"
-    database      db_name
-    datasource    datasource
-    cookbook      'app_tomcat'
+  log "  Creating context.xml for DB: #{db_name} using adapter #{db_adapter} and datasource #{node[:app_tomcat][:datasource_name]}"
+  if db_adapter == "mysql"
+    db_mysql_connect_app "/etc/tomcat6/context.xml" do
+      template      "context_xml.erb"
+      owner         "#{node[:app_tomcat][:app_user]}"
+      group         "root"
+      mode          "0644"
+      database      db_name
+      cookbook      'app_tomcat'
+    end
+  elsif db_adapter == "postgresql"
+    db_postgres_connect_app "/etc/tomcat6/context.xml" do
+      template      "context_xml.erb"
+      owner         "#{node[:app_tomcat][:app_user]}"
+      group         "root"
+      mode          "0644"
+      database      db_name
+      cookbook      'app_tomcat'
+    end
+  else
+    raise "Unrecognized database adapter #{node[:app_tomcat][:db_adapter]}, exiting"
   end
 
   log "  Creating web.xml"
@@ -339,6 +350,9 @@ action :setup_db_connection do
     owner "#{node[:app_tomcat][:user]}"
     group "root"
     mode "0644"
+    variables(
+      :datasource => node[:app_tomcat][:datasource_name]
+    )
     cookbook 'app_tomcat'
   end
 
