c1a2b0187e105bba0a79a9cbd011c5dfa0b2271d
Nitin Mohan <nitin@rightscale.com>
2012-07-06 21:13:03 +0000

    [lb_haproxy] Remove deletion of haproxy.cfg and symlink rightscale_lb.cfg to haproxy.cfg

--- cookbooks/lb_haproxy/providers/default.rb
+++ cookbooks/lb_haproxy/providers/default.rb
@@ -80,18 +80,6 @@ action :install do
     umask 0077
     notifies :start, resources(:service => "haproxy")
   end
-
-  # Remove haproxy config file so we can symlink it.
-  file "/etc/haproxy/haproxy.cfg" do
-    backup false
-    not_if { ::File.symlink?("/etc/haproxy/haproxy.cfg") }
-    action :delete
-  end
-
-  # Symlink haproxy config.
-  link "/etc/haproxy/haproxy.cfg" do
-    to "/etc/haproxy/rightscale_lb.cfg"
-  end
 end
 
 
