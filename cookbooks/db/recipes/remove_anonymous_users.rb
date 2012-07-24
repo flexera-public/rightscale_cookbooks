bb55a2cceae53e203abddf7e82f56d65531f3cd6
Nitin Mohan <nitin@rightscale.com>
2012-07-24 00:30:05 +0000

    [db] Add info about anonymous users and why they should be removed

--- cookbooks/db/recipes/remove_anonymous_users.rb
+++ cookbooks/db/recipes/remove_anonymous_users.rb
@@ -5,6 +5,14 @@
 # RightScale Terms of Service available at http://www.rightscale.com/terms.php and,
 # if applicable, other agreements such as a RightScale Master Subscription Agreement.
 
+# Anonymous users are created by default by MySQl to allow users access the database
+# without using a username and password.
+# For more information, visit 
+# http://dev.mysql.com/doc/refman/5.5/en/default-privileges.html
+
+# This recipe removes anonymous users acces from any hosts except localhost to
+# prevent remote unauthorized access.
+
 rightscale_marker :begin
 
 DATA_DIR = node[:db][:data_dir]
