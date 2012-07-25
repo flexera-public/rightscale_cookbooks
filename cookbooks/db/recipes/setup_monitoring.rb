7496f33e31889abdc431ff8315c0706d2a6aec51
mrusan <maksim.rusan@rightscale.com>
2012-07-25 13:17:52 +0300

    [db::setup_monitoring] developing backup info file

--- cookbooks/db/recipes/setup_monitoring.rb
+++ cookbooks/db/recipes/setup_monitoring.rb
@@ -11,4 +11,27 @@ db node[:db][:data_dir] do
   action :setup_monitoring
 end
 
+# Creating a db backup info file
+file "/mnt/storage/db_sys_info.log" do
+  owner "root"
+  group "root"
+  mode "0755"
+  action :create
+end
+
+execute "uname" do
+  command "echo \"System information:\n\" > /mnt/storage/db_sys_info.log && uname -a >> /mnt/storage/db_sys_info.log"
+  action :run
+end
+
+execute "lsb_release" do
+  command "lsb_release -a >> /mnt/storage/db_sys_info.log"
+  action :run
+end
+
+execute "mysql" do
+  command "echo \"\nMySQL information:\n\" >> /mnt/storage/db_sys_info.log && mysql -V >> /mnt/storage/db_sys_info.log"
+  action :run
+end
+
 rightscale_marker :end
