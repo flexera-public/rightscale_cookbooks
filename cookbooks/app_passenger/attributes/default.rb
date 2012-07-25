bdf9b67359ee91a729b3c265a43e44b5b0672c8c
Kannan Manickam <kannan@rightscale.com>
2012-07-25 10:19:30 -0700

    [app_passenger] - removed comments mentioning ruby EE

--- cookbooks/app_passenger/attributes/default.rb
+++ cookbooks/app_passenger/attributes/default.rb
@@ -28,11 +28,11 @@ case node[:platform]
     raise "Unrecognized distro #{node[:platform]}, exiting "
 end
 
-# Path to Ruby EE gem directory
+# Path to Ruby gem directory
 set[:app_passenger][:ruby_gem_base_dir]="/usr/lib64/ruby/gems/1.8"
-# Path to Ruby EE gem executable
+# Path to Ruby gem executable
 set[:app_passenger][:gem_bin]="/usr/bin/gem"
-# Path to Ruby EE ruby executable
+# Path to Ruby ruby executable
 set[:app_passenger][:ruby_bin]="/usr/bin/ruby"
 # Path to passenger module for apache
 set[:app_passenger][:apache_psr_install_module]="/usr/bin/passenger-install-apache2-module"
