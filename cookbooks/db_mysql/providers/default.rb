512f4d7800ae8368d33136979c0e398b3b679d31
Douglas Thrift <douglas.thrift@rightscale.com>
2012-07-27 21:35:09 +0000

    [db_mysql] Fix MySQL memory tuning and make a innodb_log_file_size parameter for the my.cnf template.

--- cookbooks/db_mysql/providers/default.rb
+++ cookbooks/db_mysql/providers/default.rb
@@ -55,7 +55,7 @@ end
 action :firewall_update_request do
   sys_firewall "Sending request to open port 3306 (MySQL) allowing this server to connect" do
     machine_tag new_resource.machine_tag
-    port 3306 
+    port 3306
     enable new_resource.enable
     ip_addr new_resource.ip_addr
     action :update_request
@@ -65,7 +65,7 @@ end
 action :firewall_update do
   sys_firewall "Opening port 3306 (MySQL) for tagged '#{new_resource.machine_tag}' to connect" do
     machine_tag new_resource.machine_tag
-    port 3306 
+    port 3306
     enable new_resource.enable
     action :update
   end
@@ -109,53 +109,50 @@ end
 action :post_restore_cleanup do
   # Performs checks for snapshot compatibility with current server
 
-  innodb_log_files_resize_required = false
-
-  ruby_block "validate_backup" do
-    block do
-      master_info = RightScale::Database::MySQL::Helper.load_replication_info(node)
-      # Check version matches
-      # Not all 11H2 snapshots (prior to 5.5 release) saved provider or version.  
-      # Assume MySQL 5.1 if nil
-      snap_version=master_info['DB_Version']||='5.1'
-      snap_provider=master_info['DB_Provider']||='db_mysql'
-      current_version= node[:db_mysql][:version]
-      current_provider=master_info['DB_Provider']||=node[:db][:provider]
-      Chef::Log.info "  Snapshot from #{snap_provider} version #{snap_version}"
-      # skip check if restore version check is false
-      if node[:db][:backup][:restore_version_check] == "true"
-        raise "FATAL: Attempting to restore #{snap_provider} #{snap_version} snapshot to #{current_provider} #{current_version} with :restore_version_check enabled." unless ( snap_version == current_version ) && ( snap_provider == current_provider )
-      else
-        Chef::Log.info "  Skipping #{provider} restore version check"
-      end
-
-      # create symlink from package default mysql datadir to restored datadir
-      default_datadir = "/var/lib/mysql"
-      unless ::File.symlink?(default_datadir)
-        FileUtils.rm_rf(default_datadir)
-        File.symlink(node[:db][:data_dir], default_datadir)
-      end
-
-      # compare size of node[:db_mysql][:tunable][:innodb_log_file_size] to
-      # actual size of restored /var/lib/mysql/ib_logfile0 (symlink)
-      innodb_log_file_size_to_bytes = case node[:db_mysql][:tunable][:innodb_log_file_size]
-                                        when /^(\d+)[Kk]$/ then $1.to_i * 1024
-                                        when /^(\d+)[Mm]$/ then $1.to_i * 1024**2
-                                        when /^(\d+)[Gg]$/ then $1.to_i * 1024**3
-                                        when /^(\d+)$/ then $1
-                                        else raise "FATAL: unknown log file size"
-                                      end
-
-      # if sizes do not match, must re-create my.cnf, start mysqld, stop, then delete files
-      if ::File.stat("/var/lib/mysql/ib_logfile0").size == innodb_log_file_size_to_bytes
-        Chef::Log.info "  innodb log file sizes the same...keeping file"
-      else
-        innodb_log_files_resize_required = true
-      end
+  master_info = RightScale::Database::MySQL::Helper.load_replication_info(node)
+  # Check version matches
+  # Not all 11H2 snapshots (prior to 5.5 release) saved provider or version.
+  # Assume MySQL 5.1 if nil
+  snap_version=master_info['DB_Version']||='5.1'
+  snap_provider=master_info['DB_Provider']||='db_mysql'
+  current_version= node[:db_mysql][:version]
+  current_provider=master_info['DB_Provider']||=node[:db][:provider]
+  Chef::Log.info "  Snapshot from #{snap_provider} version #{snap_version}"
+  # skip check if restore version check is false
+  if node[:db][:backup][:restore_version_check] == "true"
+    unless (snap_version == current_version) && (snap_provider == current_provider)
+      raise "FATAL: Attempting to restore #{snap_provider} #{snap_version} snapshot to #{current_provider} #{current_version} with :restore_version_check enabled."
     end
+  else
+    Chef::Log.info "  Skipping #{provider} restore version check"
   end
 
-  if innodb_log_files_resize_required
+  # create symlink from package default mysql datadir to restored datadir
+  default_datadir = "/var/lib/mysql"
+  unless ::File.symlink?(default_datadir)
+    FileUtils.rm_rf(default_datadir)
+    File.symlink(node[:db][:data_dir], default_datadir)
+  end
+
+  # compare size of node[:db_mysql][:tunable][:innodb_log_file_size] to
+  # actual size of restored /var/lib/mysql/ib_logfile0 (symlink)
+  innodb_log_file_size_to_bytes = case node[:db_mysql][:tunable][:innodb_log_file_size]
+  when /^(\d+)[Kk]$/
+    $1.to_i * 1024
+  when /^(\d+)[Mm]$/
+    $1.to_i * 1024**2
+  when /^(\d+)[Gg]$/
+    $1.to_i * 1024**3
+  when /^(\d+)$/
+    $1
+  else
+    raise "FATAL: unknown log file size"
+  end
+
+  # if sizes do not match, must re-create my.cnf, start mysqld, stop, then delete files
+  if ::File.stat("/var/lib/mysql/ib_logfile0").size == innodb_log_file_size_to_bytes
+    Chef::Log.info "  innodb log file sizes the same...keeping file"
+  else
     # recreate my.cnf
     db_mysql_set_mycnf "setup_mycnf" do
       server_id RightScale::Database::MySQL::Helper.mycnf_uuid(node)
@@ -356,7 +353,7 @@ action :install_server do
   # Timeouts enabled.
   # Ubuntu's init script does not support configurable startup timeout
   #
-  log_msg = ( platform =~ /redhat|centos/ ) ?  "  Setting mysql startup timeout" : "  Skipping mysql startup timeout setting for Ubuntu" 
+  log_msg = ( platform =~ /redhat|centos/ ) ?  "  Setting mysql startup timeout" : "  Skipping mysql startup timeout setting for Ubuntu"
   log log_msg
   template "/etc/sysconfig/#{node[:db_mysql][:service_name]}" do
     source "sysconfig-mysqld.erb"
@@ -488,7 +485,7 @@ action :promote do
     server_id RightScale::Database::MySQL::Helper.mycnf_uuid(node)
     relay_log RightScale::Database::MySQL::Helper.mycnf_relay_log(node)
   end
-  
+
   db node[:db][:data_dir] do
     action :start
     persist false
@@ -653,7 +650,7 @@ action :enable_replication do
       master_info = RightScale::Database::MySQL::Helper.load_replication_info(node)
       newmaster_host = master_info['Master_IP']
       newmaster_logfile = master_info['File']
-      newmaster_position = master_info['Position'] 
+      newmaster_position = master_info['Position']
       RightScale::Database::MySQL::Helper.reconfigure_replication(node, 'localhost', newmaster_host, newmaster_logfile, newmaster_position)
     end
   end
@@ -665,7 +662,7 @@ action :enable_replication do
       master_info = RightScale::Database::MySQL::Helper.load_master_info_file(node)
       newmaster_host = node[:db][:current_master_ip]
       newmaster_logfile = master_info['File']
-      newmaster_position = master_info['Position'] 
+      newmaster_position = master_info['Position']
       RightScale::Database::MySQL::Helper.reconfigure_replication(node, 'localhost', newmaster_host, newmaster_logfile, newmaster_position)
     end
   end
@@ -693,7 +690,7 @@ action :generate_dump_file do
 end
 
 action :restore_from_dump_file do
- 
+
   db_name   = new_resource.db_name
   dumpfile  = new_resource.dumpfile
   db_check  = `mysql -e "SHOW DATABASES LIKE '#{db_name}'"`
@@ -706,18 +703,18 @@ action :restore_from_dump_file do
       end
     end
   end
-  
+
   bash "Import MySQL dump file: #{dumpfile}" do
     only_if { db_check.empty? }
     user "root"
     flags "-ex"
     code <<-EOH
-      if [ ! -f #{dumpfile} ] 
-      then 
-        echo "ERROR: MySQL dumpfile not found! File: '#{dumpfile}'" 
+      if [ ! -f #{dumpfile} ]
+      then
+        echo "ERROR: MySQL dumpfile not found! File: '#{dumpfile}'"
         exit 1
-      fi 
-      mysqladmin -u root create #{db_name} 
+      fi
+      mysqladmin -u root create #{db_name}
       gunzip < #{dumpfile} | mysql -u root -b #{db_name}
     EOH
   end
