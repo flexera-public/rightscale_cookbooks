044114df5efac36a818fbbf3530176428362e970
Neve <n.stakanov@tut.by>
2012-08-14 15:01:55 +0300

    [lb]cleanup

--- cookbooks/lb/recipes/setup_advanced_config.rb
+++ cookbooks/lb/recipes/setup_advanced_config.rb
@@ -14,28 +14,6 @@ end
 # Prepare user data to use in LWRP
 # Example: node[:lb][:advanced_config][:backend_authorized_users] = "/serverid{admin:123, admin2:345}; /appserver{user1:678}"
 if node[:lb][:advanced_config][:backend_authorized_users]
-  base_string =node[:lb][:advanced_config][:backend_authorized_users]
-  user_arr = base_string.gsub(/\s+/, "").split(";")
-  cred_store = Hash.new
-
-  user_arr.each do |record|
-    backend_name = record[/(.+)\{/][$1]  # => /serverid
-    users = record[/\{(.+)\}/][$1] # -> admin:123, admin2:345
-    user_array = users.split "," # -> ["admin:123","admin2:345"]
-    cred_store["#{backend_name}"]= user_array
-
-    backend_short_name = backend_name.gsub(/[\/]/, '_')
-
-    lb backend_short_name do
-      backend_authorized_users cred_store["#{backend_name}"]
-      action :advanced_configs
-    end
-
-  end
-end
-
-
-if node[:lb][:advanced_config][:backend_authorized_users]
   base_string = node[:lb][:advanced_config][:backend_authorized_users]
   entry_items = base_string.gsub(/\s+/, "").split(";")
 
