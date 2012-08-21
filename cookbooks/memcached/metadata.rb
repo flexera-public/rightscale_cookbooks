b144d38cba8e6e0882057603561a52ca69e5cb04
Ravi Bhure <ravibhure@gmail.com>
2012-08-21 06:54:56 -0400

    rake metadata

--- cookbooks/memcached/metadata.rb
+++ cookbooks/memcached/metadata.rb
@@ -6,7 +6,6 @@ long_description IO.read(File.join(File.dirname(__FILE__), 'README.rdoc'))
 version          "13.1.0"
 
 supports "centos", "~> 5.8"
-supports "centos", "~> 6.2"
 supports "redhat", "~> 5.8"
 supports "ubuntu", "~> 10.04.0"
 
