1c401e6d1c883e41bcba6d63c1d49ef3afb6d7ce
Neve <n.stakanov@tut.by>
2012-09-06 15:23:53 +0300

    [db] add conditional to prevent provider redefine on DBMgr

--- cookbooks/db/recipes/install_client.rb
+++ cookbooks/db/recipes/install_client.rb
@@ -7,8 +7,12 @@
 
 rightscale_marker :begin
 database_type = node[:db][:database_adapter].match(/(^[a-z]+)_(\d.\d)/)
+
 # Database provider type Ex: db_mysql
-node[:db][:provider] = "db_#{database_type[0]}"
+# need this conditional to avoid database type collisions on DBMGR
+if node[:db][:provider] == ""
+  node[:db][:provider] = "db_#{database_type[0]}"
+end
 # Database version number Ex: 5.1
 database_version = database_type[1]
 
