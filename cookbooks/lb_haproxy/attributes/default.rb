bd61c1f4142dce908dad8611888afa0281bd0ece
Ryan O'Leary <ryan.oleary@rightscale.com>
2012-09-17 11:56:10 -0700

    Corrected cookbook name in comments

--- cookbooks/lb_haproxy/attributes/default.rb
+++ cookbooks/lb_haproxy/attributes/default.rb
@@ -1,5 +1,5 @@
 # 
-# Cookbook Name:: lb
+# Cookbook Name:: lb_haproxy
 #
 # Copyright RightScale, Inc. All rights reserved.  All access and use subject to the
 # RightScale Terms of Service available at http://www.rightscale.com/terms.php and,
