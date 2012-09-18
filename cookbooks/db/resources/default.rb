616f5fa932a6e641e178b2f080d5247ccb0780fa
Nitin Mohan <nitin@rightscale.com>
2012-09-18 23:27:58 +0000

    [db] Replace add_action method with actions in DB resource.

--- cookbooks/db/resources/default.rb
+++ cookbooks/db/resources/default.rb
@@ -5,14 +5,6 @@
 # RightScale Terms of Service available at http://www.rightscale.com/terms.php and,
 # if applicable, other agreements such as a RightScale Master Subscription Agreement.
 
-# Add actions to @action_list array.
-# Used to allow comments between entries.
-def self.add_action(sym)
-  @action_list ||= Array.new
-  @action_list << sym unless @action_list.include?(sym)
-  @action_list
-end
-
 # = Database Attributes
 #
 # Below are the attributes defined by the db resource interface.
@@ -56,14 +48,14 @@ attribute :db_version, :kind_of => String
 #
 # Calls the correct init.d script for the database and platform.
 #
-add_action :stop
+actions :stop
 
 # == Start
 # Start the database service.
 #
 # Calls the correct init.d script for the database and platform.
 #
-add_action :start
+actions :start
 
 # == Status
 # Log the status of the database service.
@@ -71,14 +63,14 @@ add_action :start
 # Calls the correct init.d script for the database and platform
 # and send the output to the Chef log and RightScale audit entries.
 #
-add_action :status
+actions :status
 
 # == Lock
 # Lock the database so writes will be blocked.
-#
+
 # This must insure a consistent state while taking a snapshot.
 #
-add_action :lock
+actions :lock
 
 # == Unlock
 # Unlock the database so writes can occur.
@@ -86,7 +78,7 @@ add_action :lock
 # This must be called as soon as possible after calling the :lock action
 # since clients will be blocked from writing until unlocked.
 #
-add_action :unlock
+actions :unlock
 
 # == Reset
 # Wipes the current database into a pristine state.
@@ -96,17 +88,17 @@ add_action :unlock
 #
 # WARNING: this will delete any data in your database!
 #
-add_action :reset
+actions :reset
 
 # == Firewall Update
 # Updates database firewall rules.
 #
-add_action :firewall_update
+actions :firewall_update
 
 # == Firewall Update Request
 # Sends a remote_recipe that requests a database updates it's firewall rules.
 #
-add_action :firewall_update_request
+actions :firewall_update_request
 
 # == Move Data Directory
 # Relocate the database data directory
@@ -119,13 +111,13 @@ add_action :firewall_update_request
 # This action should also setup a symlink from the old path to the new
 # location.
 #
-add_action :move_data_dir
+actions :move_data_dir
 
 # == Generate dump file
-add_action :generate_dump_file
+actions :generate_dump_file
 
 # == restore db from dump file
-add_action :restore_from_dump_file
+actions :restore_from_dump_file
 
 # == Pre-backup Check
 # Verify the database is in a good state for taking a snapshot.
@@ -136,7 +128,7 @@ add_action :restore_from_dump_file
 # This action should raise an exception if the database is not
 # in a valid state for a backup.
 #
-add_action :pre_backup_check
+actions :pre_backup_check
 
 # == Post-backup Cleanup
 # Used to cleanup VM after backup.
@@ -144,7 +136,7 @@ add_action :pre_backup_check
 # This action is called after the backup has completed.  Can be used to cleanup
 # any temporary files created from the :pre_backup_check action.
 #
-add_action :post_backup_cleanup
+actions :post_backup_cleanup
 
 # == Write Backup Info
 # Write backup information needed during restore.
@@ -153,7 +145,7 @@ add_action :post_backup_cleanup
 # It contains information about the current DB setup (db provider, version, replication
 # details, etc.) that is used during restore to verify the backup and initialize
 # the DB. The file is written to the DB data block device and is part of the backup.
-add_action :write_backup_info
+actions :write_backup_info
 
 # == Pre-restore Check
 # Verify the database is in a good state before preforming a restore.
@@ -166,7 +158,7 @@ add_action :write_backup_info
 # This action should raise an exception if the database is not
 # in a valid state for a restore.
 #
-add_action :pre_restore_check
+actions :pre_restore_check
 
 # == Post-restore Cleanup Validation
 # Used to validate backup and cleanup VM after restore.
@@ -182,7 +174,7 @@ add_action :pre_restore_check
 # Can also be used to perform other steps necessary to cleanup after a
 # restore.
 #
-add_action :post_restore_cleanup
+actions :post_restore_cleanup
 
 # == Set Privileges
 # Set database user privileges.
@@ -190,7 +182,7 @@ add_action :post_restore_cleanup
 # Use the privilege attributes of this resource to setup 'administrator' or
 # 'user' privilege to the given username with the given password.
 #
-add_action :set_privileges
+actions :set_privileges
 
 # == Install Client
 # Installs database client
@@ -199,12 +191,12 @@ add_action :set_privileges
 # Also should install language binding packages. For example: ruby client gem
 # java client jar, php client modules, etc
 #
-add_action :install_client
+actions :install_client
 
 # == Install Server
 # Installs database server
 #
-add_action :install_server
+actions :install_server
 
 # == Setup Monitoring
 # Install and configure collectd plugins for the server.
@@ -213,25 +205,23 @@ add_action :install_server
 # on the RightScale dashboard.  Also enables alerts and escalations for the
 # database.
 #
-add_action :setup_monitoring
+actions :setup_monitoring
 
 # == Enable Replication
 # Configures and start a slave replicating from master
-add_action :enable_replication
+actions :enable_replication
 
 # == Promote
 # Promotes a slave server to the master server.
 #
 # This is called when a new master is needed.  If the prior master is still
 # functioning it is demoted and configured as a slave.
-add_action :promote
+actions :promote
 
 # == Grant Replication Slave
 # Set database replication privileges for a slave.
 #
 # This is called when a slave is initialized.
-add_action :grant_replication_slave
+actions :grant_replication_slave
 
-add_action :remove_anonymous
-actions @action_list
-
+actions :remove_anonymous
