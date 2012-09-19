dbab86e20f9df8d0b2db8aee9db33132794802e3
mrusan <maksim.rusan@rightscale.com>
2012-09-19 17:52:02 +0300

    [rightscale] changed collectd node value init order

--- cookbooks/rightscale/attributes/monitoring.rb
+++ cookbooks/rightscale/attributes/monitoring.rb
@@ -5,9 +5,6 @@
 # RightScale Terms of Service available at http://www.rightscale.com/terms.php and,
 # if applicable, other agreements such as a RightScale Master Subscription Agreement.
 
-set_unless[:rightscale][:collectd_lib] = "/usr/lib64/collectd"
-set_unless[:rightscale][:collectd_share] = "/usr/share/collectd"
-
 case platform
 when "ubuntu"
   set_unless[:rightscale][:collectd_packages] = ["collectd", "collectd-core", "collectd-utils", "libcollectdclient0"]
@@ -17,7 +14,6 @@ when "ubuntu"
   when /^10\..+/
     set_unless[:rightscale][:collectd_packages_version] = "4.10.1-2"
   when /^12\..+/
-    set_unless[:rightscale][:collectd_packages_version] = "latest"
     set_unless[:rightscale][:collectd_lib] = "/usr/lib/collectd"
   end
 when "centos", "redhat"
@@ -28,12 +24,14 @@ when "centos", "redhat"
   when /^5\..+/
     set_unless[:rightscale][:collectd_packages_version] = "4.10.0-4.el5"
     set_unless[:rightscale][:collectd_remove_existing] = true
-  when /^6\..+/
-    set_unless[:rightscale][:collectd_packages_version] = "latest"
   end
 else
   raise "Unrecognized distro #{node[:platform]} for monitoring attributes , exiting "
 end
+
+set_unless[:rightscale][:collectd_packages_version] = "latest"
+set_unless[:rightscale][:collectd_lib] = "/usr/lib64/collectd"
+set_unless[:rightscale][:collectd_share] = "/usr/share/collectd"
 
 default[:rightscale][:plugin_list] = ""
 default[:rightscale][:plugin_list_array] = [
