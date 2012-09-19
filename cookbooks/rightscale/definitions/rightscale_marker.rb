0d98d4aac6ffe5a7cd5502654ef31adfb7541128
Douglas Thrift <douglas.thrift@rightscale.com>
2012-09-19 17:54:06 +0000

    [rightscale] Change the marker to only use two regexes and a case statement instead of if/elsif/end. Also added YARD doc for it.

--- cookbooks/rightscale/definitions/rightscale_marker.rb
+++ cookbooks/rightscale/definitions/rightscale_marker.rb
@@ -5,20 +5,17 @@
 # RightScale Terms of Service available at http://www.rightscale.com/terms.php and,
 # if applicable, other agreements such as a RightScale Master Subscription Agreement.
 
+# Marks the beginning and end of a Chef recipe converge in RightScale Audit Entries and logs.
+#
+# @param [String, Symbol] name The marker to use; can be +:begin+ or +:end+. Also, +:start+ and +:stop+ will work.
 define :rightscale_marker do
 
   recipe_name = "#{self.cookbook_name}" + "::" + "#{self.recipe_name}"
 
   location = params[:name] ? params[:name] : "start"
 
-  # translate symbols to strings ie :begin = "begin"
-  location = location.to_s
-
-  # detect if used 'begin' instead of 'start' or 'stop' instead of 'end'
-  location = "start" if location =~ /^begin$/
-  location = "end" if location =~ /^stop$/
-
-  if location =~ /^start$/
+  case location.to_s
+  when /^(start|begin)$/
     # We use Chef::Log.info here to get clear output
     ruby_block "log marker" do
       block do
@@ -26,10 +23,10 @@ define :rightscale_marker do
         Chef::Log.info "*RS>  Running recipe #{recipe_name}   ****"
       end
     end
-  elsif location =~ /^end$/
+  when /^(stop|end)$/
     # Do nothing
   else
-    log "unknown marker"
+    Chef::Log.warn "unknown marker (#{location})"
   end
 
 end
