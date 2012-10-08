b8f31ae6f33772dcf462451db7e062516ebee4af
Kannan Manickam <kannan@rightscale.com>
2012-10-08 02:36:11 -0700

    [monkey] - moved attributes to metadata for inputs

--- cookbooks/monkey/attributes/default.rb
+++ cookbooks/monkey/attributes/default.rb
@@ -7,10 +7,6 @@
 
 # Recommended attributes
 #
-set_unless[:monkey][:git][:user] = "arangamani"
-set_unless[:monkey][:git][:email] = "kannan@rightscale.com"
-set_unless[:monkey][:git][:ssh_key] = "somekey"
-set_unless[:monkey][:git][:host_name] = "github.com"
 
 set[:monkey][:rest][:packages] = []
 set[:monkey][:rest][:gem_packages] = [
@@ -22,6 +18,9 @@ set[:monkey][:rest][:gem_packages] = [
   "diff-lcs",
   "rspec"
 ]
+set[:monkey][:rest][:version] = ""
+
+set[:monkey][:virtualmonkey][:packages] = []
 set[:monkey][:virtualmonkey][:gem_packages] = [
   "rake",
   "hoe",
@@ -37,14 +36,4 @@ set[:monkey][:virtualmonkey][:gem_packages] = [
   "right_http_connection",
   "right_aws"
 ]
-
-node[:monkey][:rest][:version] = ""
-node[:monkey][:rest][:right_passwd] = "passwd"
-node[:monkey][:rest][:right_email] = "email"
-node[:monkey][:rest][:right_acct_id] = "2901"
-node[:monkey][:rest][:ssh_key] = "somekey"
-
-node[:monkey][:virtualmonkey][:packages] = []
-node[:monkey][:virtualmonkey][:version]
-node[:monkey][:virtualmonkey][:collateral_repo_url] = 'git@github.com:rightscale/servertemplate_tests.git'
-node[:monkey][:virtualmonkey][:environment] = 'testing'
+set[:monkey][:virtualmonkey][:version] = ""
--- cookbooks/monkey/metadata.rb
+++ cookbooks/monkey/metadata.rb
@@ -11,8 +11,6 @@ version          "13.2.0"
 
 
 depends "rightscale"
-depends "sys_firewall"
-depends "repo"
 
 recipe "monkey::default", "Default recipe for monkey setup."
 recipe "monkey::setup_git", "Setting up Git for monkey."
@@ -32,4 +30,64 @@ attribute "monkey/git/email",
   :description => "Git email address to be used with github",
   :required => "required",
   :recipes => ["monkey::setup_git"]
+
+attribute "monkey/git/ssh_key",
+  :display_name => "SSH key for Git",
+  :description => "SSH key for using with Git",
+  :required => "required",
+  :recipes => ["monkey::setup_git"]
+
+attribute "monkey/git/host_name",
+  :display_name => "Git Hostname",
+  :description => "Git Hostname for adding to ssh config",
+  :required => "required",
+  :recipes => ["monkey::setup_git"]
+
+attribute "monkey/rest/right_passwd",
+  :display_name => "RightScale password",
+  :description => "RightScale password to connect to the API",
+  :required => "required",
+  :recipes => ["monkey::setup_rest_connection"]
+
+attribute "monkey/rest/right_email",
+  :display_name => "RightScale Email",
+  :description => "RightScale email address to connect to the API",
+  :required => "required",
+  :recipes => ["monkey::setup_rest_connection"]
+
+attribute "monkey/rest/right_acct_id",
+  :display_name => "RightScale account ID",
+  :description => "RightScale account ID used to connect to the API",
+  :required => "required",
+  :recipes => ["monkey::setup_git"]
+
+attribute "monkey/rest/ssh_key",
+  :display_name => "API user key",
+  :description => "API user key used by rest_connection",
+  :required => "required",
+  :recipes => ["monkey::setup_rest_connection"]
+
+attribute "monkey/rest/repo_url",
+  :display_name => "Rest connection Repo URL",
+  :description => "Git URL for checking out rest_connection project",
+  :required => "required",
+  :recipes => ["monkey::setup_rest_connection"]
+
+attribute "monkey/virtualmonkey/monkey_repo_url",
+  :display_name => "VirtualMonkey Repo URL",
+  :description => "Git repository URL for VirtualMonkey",
+  :required => "required",
+  :recipes => ["monkey::setup_virtualmonkey"]
+
+attribute "monkey/virtualmonkey/collateral_repo_url",
+  :display_name => "Collateral Repo URL",
+  :description => "Git URL for collateral project",
+  :required => "required",
+  :recipes => ["monkey::setup_virtualmonkey"]
+
+attribute "monkey/virtualmonkey/environment",
+  :display_name => "VirtualMonkey Environment",
+  :description => "VirtualMonkey Environmnet",
+  :required => "required",
+  :recipes => ["monkey::setup_virtualmonkey"]
 
