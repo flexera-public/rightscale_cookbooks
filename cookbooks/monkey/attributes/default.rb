2df189347b8735403302e7861fed94337b6d7ad9
Kannan Manickam <kannan@rightscale.com>
2012-10-08 01:48:57 -0700

    [monkey] added attributes

--- cookbooks/monkey/attributes/default.rb
+++ cookbooks/monkey/attributes/default.rb
@@ -7,4 +7,44 @@
 
 # Recommended attributes
 #
-set_unless[:memcached][:tcp_port] = "11211"
+set_unless[:monkey][:git][:user] = "arangamani"
+set_unless[:monkey][:git][:email] = "kannan@rightscale.com"
+set_unless[:monkey][:git][:ssh_key] = "somekey"
+set_unless[:monkey][:git][:host_name] = "github.com"
+
+set[:monkey][:rest][:packages] = []
+set[:monkey][:rest][:gem_packages] = [
+  "rake",
+  "bundler",
+  "jeweler",
+  "ruby-debug",
+  "gemedit",
+  "diff-lcs",
+  "rspec"
+]
+set[:monkey][:virtualmonkey][:gem_packages] = [
+  "rake",
+  "hoe",
+  "rcov", 
+  "fog", 
+  "ParseTree",
+  "ruby2ruby",
+  "chef",
+  "daemons",
+  "colorize",
+  "sinatra",
+  "chronic",
+  "right_http_connection",
+  "right_aws"
+]
+
+node[:monkey][:rest][:version] = ""
+node[:monkey][:rest][:right_passwd] = "passwd"
+node[:monkey][:rest][:right_email] = "email"
+node[:monkey][:rest][:right_acct_id] = "2901"
+node[:monkey][:rest][:ssh_key] = "somekey"
+
+node[:monkey][:virtualmonkey][:packages] = []
+node[:monkey][:virtualmonkey][:version]
+node[:monkey][:virtualmonkey][:collateral_repo_url] = 'git@github.com:rightscale/servertemplate_tests.git'
+node[:monkey][:virtualmonkey][:environment] = 'testing'
