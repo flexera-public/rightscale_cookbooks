c241d75ee6af571ee24774080f808a1dd8fea7fc
Nitin Mohan <nitin@rightscale.com>
2012-10-19 03:34:45 +0000

    [app] node[:app][:database_server_fqdn] - server misspelled

--- cookbooks/app/recipes/setup_db_connection.rb
+++ cookbooks/app/recipes/setup_db_connection.rb
@@ -15,7 +15,7 @@ app "default" do
   database_name        node[:app][:database_name]
   database_user        node[:app][:database_user]
   database_password    node[:app][:database_password]
-  database_sever_fqdn  node[:app][:database_sever_fqdn]
+  database_server_fqdn  node[:app][:database_server_fqdn]
   action :setup_db_connection
 end
 
