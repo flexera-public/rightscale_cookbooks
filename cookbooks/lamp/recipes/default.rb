cf6c5334447fccae01ac3aff3b60330cf0c257f8
Nitin Mohan <nitin@rightscale.com>
2012-10-26 23:31:54 +0000

    [lamp] Added navigational comments

--- cookbooks/lamp/recipes/default.rb
+++ cookbooks/lamp/recipes/default.rb
@@ -7,11 +7,10 @@
 
 rightscale_marker :begin
 
-# Set the LAMP specific node variables.
+# Set LAMP specific node variables.
 #
-# Make sure to run this recipe after the php application server default recipe
+# Make sure to run this recipe after the php application server installation recipe
 # to ensure that it does not over write these values.
-
 node[:db_mysql][:bind_address] = "localhost"
 log "  LAMP set MySQL to listen on #{node[:db_mysql][:bind_address]}"
 
@@ -19,6 +18,7 @@ node[:app][:port] = "80"
 log "  LAMP set Apache to listen on port #{node[:app][:port]}"
 
 # Setup default values for application resource and install required packages.
+# See cookbooks/app_php/providers/default.rb for "install" action.
 app "default" do
   persist true
   provider node[:app][:provider]
