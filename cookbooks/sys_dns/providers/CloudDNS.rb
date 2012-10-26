92d77734a52f72adc750f51587a4339d14bb8a91
Kannan Manickam <kannan@rightscale.com>
2012-10-26 15:04:46 -0700

    [sys_dns] - code review changes

--- cookbooks/sys_dns/providers/CloudDNS.rb
+++ cookbooks/sys_dns/providers/CloudDNS.rb
@@ -8,7 +8,7 @@
 action :set_private do
   log "==================== sys_dns(:set_private) CloudDNS BEGIN ==================== "
 
-  # Creates an instance for CloudDNS class (found in cookbooks/sys_dns/libraries/helper.rb)
+  # Creates an instance of the CloudDNS class (found in cookbooks/sys_dns/libraries/helper.rb)
   # and calls the 'action_set' method with proper parameters.
   RightScale::DnsTools::CloudDNS.new(Chef::Log).action_set(new_resource.id, new_resource.user, new_resource.password, new_resource.address, :region => new_resource.region )
 
--- cookbooks/sys_dns/providers/DNSMadeEasy.rb
+++ cookbooks/sys_dns/providers/DNSMadeEasy.rb
@@ -8,7 +8,7 @@
 action :set_private do
   log "==================== sys_dns(:set_private) DNSMadeEasy BEGIN ==================== "
 
-  # Creates an instance for DME class (found in cookbooks/sys_dns/libraries/helper.rb)
+  # Creates an instance of the DME class (found in cookbooks/sys_dns/libraries/helper.rb)
   # and calls the 'action_set' method with proper parameters.
   RightScale::DnsTools::DME.new(Chef::Log).action_set(new_resource.id, new_resource.user, new_resource.password, new_resource.address)
 
