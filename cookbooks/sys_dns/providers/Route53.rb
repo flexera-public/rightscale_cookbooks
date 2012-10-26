d06f3f7d80c86a345499a0d7458d2a394e95505b
Kannan Manickam <kannan@rightscale.com>
2012-10-26 14:10:45 -0700

    [sys_dns] - added navigational comments

--- cookbooks/sys_dns/providers/Route53.rb
+++ cookbooks/sys_dns/providers/Route53.rb
@@ -8,7 +8,7 @@
 action :set_private do
   log "==================== sys_dns(:set_private) Route53 BEGIN ==================== "
 
-  # A new instance for AWS class (found in 'cookbooks/sys_dns/libraries/helper.rb') is created
+  # A new instance for AWS class (found in cookbooks/sys_dns/libraries/helper.rb) is created
   # and the 'action_set' method is called with proper parameters.
   RightScale::DnsTools::AWS.new(Chef::Log).action_set(new_resource.id, new_resource.user, new_resource.password, new_resource.address)
 
