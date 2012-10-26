767357ab27815bba3093a3282396dc8d904aff0b
mrusan <maksim.rusan@rightscale.com>
2012-10-26 15:15:17 -0700

    [memcached] changed comment style

--- cookbooks/memcached/recipes/install_server.rb
+++ cookbooks/memcached/recipes/install_server.rb
@@ -10,12 +10,13 @@ rightscale_marker :begin
 # Installing server tags
 #
 # The instance is identified as a memcached server.
+# See http://support.rightscale.com/12-Guides/Chef_Cookbooks_Developer_Guide/Chef_Resources#RightLinkTag for "right_link_tag" definition
 right_link_tag "memcached_server:active=true"
 # The server name so that sorts can be done to get the correct order across app servers.
 right_link_tag "memcached_server:uuid=#{node[:rightscale][:instance_uuid]}"
 # The instance is associated with a cluster.
 right_link_tag "memcached_server:cluster=#{node[:memcached][:cluster_id]}"
-# The listening port.
+# The listening port tag is set.
 right_link_tag "memcached_server:port=#{node[:memcached][:tcp_port]}"
 
 log "  Server tags installed."
@@ -63,7 +64,7 @@ when "any"
   node[:memcached][:interface] = "0.0.0.0"
 end
 
-# Logging output level.
+# Determine logging output level.
 log_level = ""
 case node[:memcached][:log_level]
 when "verbose"
