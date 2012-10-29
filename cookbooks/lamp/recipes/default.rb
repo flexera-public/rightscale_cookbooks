7366875a1dc3e588d16f0400e1a7e03728402160
Nitin Mohan <nitin@rightscale.com>
2012-10-29 19:02:32 +0000

    [lamp] Updated comments.

--- cookbooks/lamp/recipes/default.rb
+++ cookbooks/lamp/recipes/default.rb
@@ -7,7 +7,7 @@
 
 rightscale_marker :begin
 
-# Set LAMP specific node variables.
+# Set the LAMP specific node variables.
 #
 # Make sure to run this recipe after the php application server installation recipe
 # to ensure that it does not over write these values.
@@ -18,7 +18,7 @@ node[:app][:port] = "80"
 log "  LAMP set Apache to listen on port #{node[:app][:port]}"
 
 # Setup default values for application resource and install required packages.
-# See cookbooks/app_php/providers/default.rb for "install" action.
+# See cookbooks/app_php/providers/default.rb for the "install" action.
 app "default" do
   persist true
   provider node[:app][:provider]
