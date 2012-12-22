0cfe06f9ee50ed8ffeee3d0a0466fcced061bcad
Nitin Mohan <nitin@rightscale.com>
2012-12-22 06:47:25 +0000

    acu69949 - [block_device/providers] Syntax error fix

--- cookbooks/block_device/providers/default.rb
+++ cookbooks/block_device/providers/default.rb
@@ -20,7 +20,7 @@ action :create do
     :force => new_resource.force
   }
   create_options[:iops] = new_resource.iops if new_resource.iops && !new_resource.iops.empty?
-  create_options[:volume_type] => new_resource.device_type
+  create_options[:volume_type] = new_resource.device_type
   # See rightscale_tools gem for implementation of "create" method.
   device.create(create_options)
 end
