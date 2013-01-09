2ec338015e72287decd3ea8a2656c58f0795641f
Neve <nick.s@rightscale.com>
2013-01-09 12:56:52 +0300

    acu67385 - [rightscale::server_collection] code review fixes

--- cookbooks/rightscale/resources/server_collection.rb
+++ cookbooks/rightscale/resources/server_collection.rb
@@ -11,15 +11,20 @@ actions :load
 
 # Attribute specifies the tag or tags to search for (if multiple tags are specified servers with any of the tags will match).
 attribute :tags, :kind_of => [String, Array]
+
 # Optional attribute specifies tags that need to also appear on the servers.
 attribute :mandatory_tags, :kind_of => [String, Array]
-# Optional attribute duplicate of :mandatory_tags attribute
+
+# @deprecated Duplicate of +mandatory_tags+
 # we should keep it to not to break the interface
 attribute :secondary_tags, :kind_of => [String, Array]
+
 # IDs for the servers to lookup.
 attribute :agent_ids, :kind_of => [String, Array]
+
 # Acceptable timeout on the lookup operation.
 attribute :timeout, :default => 60, :kind_of => Integer
+
 attribute :empty_ok, :default => true, :equal_to => [true, false]
 
 # Defines a default action
