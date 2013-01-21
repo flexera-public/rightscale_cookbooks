48e552bd265162e4c162dfd13dbdfc9df6b03785
Neve <nick.s@rightscale.com>
2013-01-21 14:33:47 +0300

    acu72007 - [sys_firewall::default] nf_conntrack_max setup refactor

--- cookbooks/sys_firewall/recipes/default.rb
+++ cookbooks/sys_firewall/recipes/default.rb
@@ -34,25 +34,43 @@ end
 
 # Increase connection tracking table sizes
 #
-# Increase the value for the 'net.ipv4.netfilter.ip_conntrack_max' parameter
+# Increase the value for the 'netfilter.nf_conntrack_max' parameter
 # to avoid dropping packets on high-throughput systems.
-#
-# The ip_conntrack_max is calculated based on the RAM available on
-# the VM using this formula: ip_conntrack_max=32*n, where n is the amount
-# of RAM in MB. For the instance types greater or equal to 2GB, the value is
-# 65536.
 
+nf_module_name = value_for_platform(
+  "centos" => {
+    "default" => "net.netfilter.nf_conntrack_max"
+  },
+  "default" => "net.ipv4.ip_conntrack_max"
+)
+
+# The ip_conntrack_max is calculated based on the RAM available on
+# the VM using this formula: ip_conntrack_max=64*n, where n is the amount
+# of RAM in MB.
 GB=1024*1024
 mem_mb = node[:memory][:total].to_i/1024
-conn_max = (mem_mb >= 2*GB) ? 65536 : 32*mem_mb
+nf_conntrack_max = "#{nf_module_name} = #{64*mem_mb}"
 
-log "  Setup IP connection tracking limit of #{conn_max}"
-bash "Update net.ipv4.ip_conntrack_max" do
+log "  Setup IP connection tracking limit: #{nf_conntrack_max}"
+bash "Update #{nf_module_name}" do
   flags "-ex"
-  only_if { node[:platform] =~ /redhat|centos/ }
   code <<-EOH
-    sysctl -e -w net.ipv4.ip_conntrack_max=#{conn_max}
+    echo "#{nf_conntrack_max}" >> /etc/sysctl.conf
+    sysctl -e -p /etc/sysctl.conf > /dev/null
   EOH
+  not_if { ::File.readlines("/etc/sysctl.conf").grep("#{nf_conntrack_max}\n").any? }
+end
+
+# Update iptables config file to not to reset nf_* modules to its default values
+template "/etc/sysconfig/iptables-config" do
+  source "iptables_config.erb"
+  cookbook "sys_firewall"
+  not_if { node[:platform] == "ubuntu" }
+end
+
+# Rebuild iptables
+execute "rebuild-iptables" do
+  command "/usr/sbin/rebuild-iptables"
 end
 
 rightscale_marker :end
