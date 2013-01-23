fed103c2d85e152a4be3eba445cde0bb4f028df2
Nitin Mohan <nitin@rightscale.com>
2013-01-23 01:12:39 +0000

    acu74223 - [app_tomcat] Install OpenJDK 6 on both tomcat 6 and 7 to keep java version consistent.

--- cookbooks/app_tomcat/recipes/setup_server_7.rb
+++ cookbooks/app_tomcat/recipes/setup_server_7.rb
@@ -18,7 +18,8 @@ case node[:platform]
 when "centos", "redhat"
   node[:app][:user] = "tomcat"
   node[:app][:group] = "tomcat"
-  node[:app_tomcat][:configuration_file_path] = "/etc/tomcat#{version}/tomcat#{version}.conf"
+  node[:app_tomcat][:configuration_file_path] =
+    "/etc/tomcat#{version}/tomcat#{version}.conf"
   node[:app_tomcat][:jkworkersfile] = "/etc/tomcat#{version}/workers.properties"
 when "ubuntu"
   node[:app][:user] = "tomcat7"
@@ -33,6 +34,7 @@ end
 node[:app][:packages] = value_for_platform(
   ["centos", "redhat"] => {
     "default" => [
+      "java-1.6.0-openjdk",
       "eclipse-ecj",
       "ecj3",
       "tomcat7",
@@ -43,6 +45,7 @@ node[:app][:packages] = value_for_platform(
   },
   "ubuntu" => {
     "default" => [
+      "openjdk-6-jre-headless",
       "ecj-gcj",
       "tomcat7",
       "tomcat7-admin",
@@ -53,6 +56,8 @@ node[:app][:packages] = value_for_platform(
   }
 )
 
-raise "Unrecognized distro #{node[:platform]} for tomcat#{version}, exiting " if node[:app][:packages].empty?
+if node[:app][:packages].empty?
+  raise "Unrecognized distro #{node[:platform]} for tomcat#{version}, exiting"
+end
 
 rightscale_marker :end
