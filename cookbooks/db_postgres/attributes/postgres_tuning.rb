8cde7a1fa8a15434303c13725455f45210d7541f
Erik Kargard <erik@rightscale.com>
2013-01-24 21:28:15 +0000

    [db_postgres]acu70986/w5326 changed shared/dedicated symbols to a string

--- cookbooks/db_postgres/attributes/postgres_tuning.rb
+++ cookbooks/db_postgres/attributes/postgres_tuning.rb
@@ -6,9 +6,6 @@
 # if applicable, other agreements such as a RightScale Master Subscription Agreement.
 
 
-default[:db_postgres][:tunable][:shared_buffers] = "24MB"
-default[:db_postgres][:tunable][:max_connections] = "100"
-
 def value_with_units(value, units, usage_factor)
   raise "Error: value must convert to an integer." unless value.to_i
   raise "Error: units must be k, m, g" unless units =~ /[KMG]/i
@@ -20,10 +17,13 @@ end
 # Set tuning parameters.
 
 default[:db_postgres][:tunable][:ulimit] = `sysctl -n fs.file-max`.to_i/33
+default[:db_postgres][:tunable][:shared_buffers] = "24MB"
+default[:db_postgres][:tunable][:max_connections] = "100"
+
 
 # Shared servers get %50 of the resources allocated to a dedicated server.
 usage = 1 # Dedicated server
-usage = 0.5 if db_postgres[:server_usage] == :shared
+usage = 0.5 if db_postgres[:server_usage] == "shared"
 
 # Ohai returns total in KB.  Set GB so X*GB can be used in conditional
 # GB=1024*1024
@@ -39,26 +39,26 @@ if attribute?("ec2")
   # tune the database for dedicated vs. shared and instance type
   case ec2[:instance_type]
   when "t1.micro"
-    if (db_postgres[:server_usage] == :dedicated)
+    if (db_postgres[:server_usage] == "dedicated")
       default[:db_postgres][:tunable][:shared_buffers] = "48M"
     else
       default[:db_postgres][:tunable][:shared_buffers] = "24M"
       default[:db_postgres][:tunable][:max_connections] = "100"
     end
   when "m1.small", "c1.medium"
-    if (db_postgres[:server_usage] == :dedicated)
+    if (db_postgres[:server_usage] == "dedicated")
       default[:db_postgres][:tunable][:shared_buffers] = "128M"
     else
       default[:db_postgres][:tunable][:shared_buffers] = "64M"
     end
   when "m1.large", "c1.xlarge"
-    if (db_postgres[:server_usage] == :dedicated)
+    if (db_postgres[:server_usage] == "dedicated")
       default[:db_postgres][:tunable][:shared_buffers] = "192M"
     else
       default[:db_postgres][:tunable][:shared_buffers] = "128M"
     end
   when "m1.xlarge"
-    if (db_postgres[:server_usage] == :dedicated)
+    if (db_postgres[:server_usage] == "dedicated")
       default[:db_postgres][:tunable][:shared_buffers] = "265M"
     else
       default[:db_postgres][:tunable][:shared_buffers] = "192M"
