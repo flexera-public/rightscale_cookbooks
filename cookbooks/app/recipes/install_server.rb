d48c132f3d68638efe1fe3bda47fa89a6df90eb5
Kannan Manickam <kannan@rightscale.com>
2013-02-19 13:56:10 -0800

    acu78412 - [app::install_server] - verify if the IP is valid before using it in tags

--- cookbooks/app/recipes/install_server.rb
+++ cookbooks/app/recipes/install_server.rb
@@ -6,16 +6,19 @@
 # if applicable, other agreements such as a RightScale Master Subscription Agreement.
 
 rightscale_marker :begin
+
 # Set ip address that the application service is listening on.
 # If instance has no public ip's first private ip will be used.
 # User will be notified.
 public_ip = node[:cloud][:public_ips][0]
 private_ip = node[:cloud][:private_ips][0]
-if public_ip
+if RightScale::Utils::Helper.is_valid_ip?(public_ip)
   node[:app][:backend_ip_type] == "Public" ? node[:app][:ip] = public_ip : node[:app][:ip] = private_ip
-else
+elsif RightScale::Utils::Helper.is_valid_ip?(private_ip)
   log "  No public IP detected. Forcing to first private: #{private_ip}"
   node[:app][:ip] = private_ip
+else
+  raise "No valid public/private IP found for the server."
 end
 
 log "  Provider is #{node[:app][:provider]}"
