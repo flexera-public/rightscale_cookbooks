7ea234193c5b5c93eb3a3e3a4a07fca67bee1429
Douglas Thrift <douglas.thrift@rightscale.com>
2013-03-07 00:45:50 +0000

    acu81771 - Add comments according to Jon's review.

--- tests/tests/base.rb
+++ tests/tests/base.rb
@@ -1,79 +1,107 @@
-# suite variables: server_template_type = {chef, rsb}
-
 # Include the helper objects and methods.
 require_helper "cloud"
 require_helper "ephemeral"
 require_helper "wait_for_ip_repopulation"
 
+# Helpers specific to the Base test.
+#
 helpers do
-  # Checks if the swapfile is listed in /proc/swaps.
+  # Checks if the swap file is set up.
   #
-  # @param server [Server] Server's swapspace to check.
+  # @param server [Server] the server to check
   #
-  # @return [Boolean] True if swapfile is listed.
+  # @raise [RuntimeError] if the swap file is not set up
   #
-  # @raises [RuntimeError] If swap file cannot be setup.
-  def test_swapspace(server)
-    # Get location of swapfile from ServerTemplate input.
-    swapfile = get_input_from_server(server)["sys/swap_file"].to_s.split("text:")[1]
+  def check_swap_file(server)
+    # Get location of swap file from ServerTemplate input.
+    swapfile = get_input_from_server(server)["sys/swap_file"].split("text:")[1]
 
+    # SSH to the server and see if the swap file path is in the swap devices
+    # list.
+    #
     probe(servers.first, "grep -c #{swapfile} /proc/swaps") do |result, status|
       puts "Swapfile: #{result.inspect}"
+      # TODO: raise some sort of exception in the monkey exception hierarchy
       raise "raise swap file not setup correctly" unless ((result).to_i > 0)
       true
     end
   end
 
-  # Checks the server template uses Chef.
-  # Will be deprecated after features are ported to RSB.
+  # Checks if this test is running against a Chef ServerTemplate or an RSB
+  # ServerTemplate.
   #
-  # @return [Boolean] True if server template uses chef and False otherwise.
+  # @return [Boolean] whether the test is running against a Chef ServerTemplate
+  #
   def is_chef?
     suite_variables[:server_template_type] == "chef"
   end
 end
 
+# When rerunning a test, shutdown all of the servers.
+#
 hard_reset do
   stop_all
 end
 
+# Before all of the test cases, launch all of the servers in the deployment.
+#
 before do
   launch_all
   wait_for_all("operational")
 end
 
+# The Base smoke test makes sure the Base (Chef or RSB) ServerTemplate has its
+# basic functionality including setting up any ephemeral volumes, setting up a
+# swap file, and basic monitoring. It checks if this functionality is working
+# after initial boot and then after a single reboot.
+#
 test_case "smoke_test" do
-  # Current cloud.
+  # Get current cloud.
   cloud = Cloud.factory
 
   # Single server in deployment.
   server = servers.first
 
+  # Ephemeral and swap file support are currently only implemented on the Chef
+  # ServerTemplate.
+  #
   if is_chef?
     check_ephemeral_mount(server) if cloud.supports_ephemeral?(server)
-    test_swapspace(server)
+    check_swap_file(server)
   end
 
+  # Check if the server's basic monitoring is working.
   check_monitoring
 
+  # Reboot to check if functionality works after a reboot.
   reboot_all
   wait_for_all("operational")
 
+  # Ephemeral and swap file support are currently only implemented on the Chef
+  # ServerTemplate.
+  #
   if is_chef?
     check_ephemeral_mount(server) if cloud.supports_ephemeral?(server)
-    test_swapspace(server)
+    check_swap_file(server)
   end
 
+  # Check if the server's basic monitoring is working.
   check_monitoring
 end
 
+# The Base stop/start test makes sure the Base (Chef or RSB) ServerTemplate has
+# its basic functionality after a server has been stopped and then started
+# again. The functionality it tests includes setting up any ephemeral volumes,
+# setting up a swap file, and basic monitoring.
+#
 test_case "stop_start" do
-  # Current cloud.
+  # Get the current cloud.
   cloud = Cloud.factory
 
   # Single server in deployment.
   server = servers.first
 
+  # Only some clouds support stop/start.
   skip unless cloud.supports_start_stop?(server)
 
   # Stop server.
@@ -88,7 +116,19 @@ test_case "stop_start" do
   # It takes about a minute for ips to get repopulated.
   wait_for_ip_repopulation(server)
 
+  # Remove the public IP address tag if the cloud requires SSHing on the private
+  # IP address.
+  #
   server.remove_tags ["server:public_ip_0=#{server.public_ip}"] if cloud.needs_private_ssh?
-  check_ephemeral_mount(server) if cloud.supports_ephemeral?(server)
+
+  # Ephemeral and swap file support are currently only implemented on the Chef
+  # ServerTemplate.
+  #
+  if is_chef?
+    check_ephemeral_mount(server) if cloud.supports_ephemeral?(server)
+    check_swap_file(server)
+  end
+
+  # Check if the server's basic monitoring is working.
   check_monitoring
 end
