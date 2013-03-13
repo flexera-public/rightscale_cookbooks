99e8cbe4bf20a5bb73006d8d1c501371f0a83194
Douglas Thrift <douglas.thrift@rightscale.com>
2013-03-13 21:13:13 +0000

    acu81771 - Clarify the AssertionError comment and document the Cloud constructor.

--- tests/helpers/errors.rb
+++ tests/helpers/errors.rb
@@ -1,4 +1,5 @@
-# An error meaning the VirtualMonkey framework has done something unexpected.
+# An error meaning the VirtualMonkey framework or test collateral has done
+# something unexpected.
 #
 class AssertionError < VirtualMonkey::TestCase::ErrorBase
 end
