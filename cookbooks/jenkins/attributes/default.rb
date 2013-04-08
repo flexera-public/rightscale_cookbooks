02445e1534ef260ee0fc763d825a727d24563aaa
Kannan Manickam <kannan@rightscale.com>
2013-04-08 09:06:52 -0700

    acu69322 - [jenkins] - updated attributes to support running from vagrant

--- cookbooks/jenkins/attributes/default.rb
+++ cookbooks/jenkins/attributes/default.rb
@@ -8,16 +8,24 @@
 # Recommended attributes
 #
 
-default[:jenkins][:ip] = node[:cloud][:public_ips][0]
+# Attributes for Jenkins master
+
+if node[:cloud]
+  default[:jenkins][:ip] = node[:cloud][:public_ips][0]
+else
+  # To support running the use of this cookbook inside a vagrant box
+  default[:jenkins][:ip] = "127.0.0.1"
+end
 default[:jenkins][:server][:home] = "/var/lib/jenkins"
 default[:jenkins][:server][:system_user] = "jenkins"
 default[:jenkins][:server][:system_group] = "root"
 default[:jenkins][:server][:port] = "8080"
+default[:jenkins][:mirror] = "http://updates.jenkins-ci.org"
+
+# Attributes for Jenkins slave
 
 default[:jenkins][:slave][:mode] = "normal"
 default[:jenkins][:slave][:executors] = "10"
 default[:jenkins][:slave][:private_key_file] = "/root/.ssh/api_user_key"
 default[:jenkins][:slave][:attach_status] = "unattached"
-
-default[:jenkins][:mirror] = "http://updates.jenkins-ci.org"
 default[:jenkins][:attach_slave_at_boot] == "false"
