e4ae9573238e875befb5f2e2c3c959cfdcc90114
Erik Kargard <erik@rightscale.com>
2013-04-09 23:51:04 +0000

    [rightscale] acu50929 Linux Security updates recipes

---
+++ cookbooks/rightscale/recipes/setup_security_updates.rb
@@ -0,0 +1,20 @@
+#
+# Cookbook Name:: rightscale
+#
+# Copyright RightScale, Inc. All rights reserved.  All access and use subject to the
+# RightScale Terms of Service available at http://www.rightscale.com/terms.php and,
+# if applicable, other agreements such as a RightScale Master Subscription Agreement.
+
+rightscale_marker :begin
+
+if "#{node[:rightscale][:security_update]}" == "Enabled"
+
+  log "  Security updates enabled."
+
+else
+
+  log "  Security updates disabled.  Skipping setup!"
+
+end
+
+rightscale_marker :end
