def47e2cdc976b56a23b2acd40bcfc720d5e0cc9
Erik Kargard <erik@rightscale.com>
2013-04-10 22:32:09 +0000

    [rightscale] acu50929 stubbed out security monitoring

--- cookbooks/rightscale/recipes/setup_security_update_monitoring.rb
+++ cookbooks/rightscale/recipes/setup_security_update_monitoring.rb
@@ -10,6 +10,7 @@ rightscale_marker :begin
 if "#{node[:rightscale][:security_update]}" == "Enabled"
 
   log "  Security updates enabled."
+  log.warn "  Security updates are not implemented."
 
 else
 
