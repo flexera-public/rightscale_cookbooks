0849ecd4efb2e10dadcb55715dcf2991dfa744d3
Erik Kargard <erik@rightscale.com>
2013-04-10 21:06:01 +0000

    [rightscale] acu50929 operational update script

--- cookbooks/rightscale/recipes/do_security_update.rb
+++ cookbooks/rightscale/recipes/do_security_update.rb
@@ -9,7 +9,28 @@ rightscale_marker :begin
 
 if "#{node[:rightscale][:security_update]}" == "Enabled"
 
-  log "  Security updates enabled."
+# TODO These should be in a helper method since this code is duplicate
+# of the setup_security
+  case node[:platform]
+  when "ubuntu"
+    bash "Apply Ubuntu security updates" do
+      flags "-ex"
+      code <<-EOH
+        # Make sure we DON'T check the output of this, as apt-get update
+        # may return a non-zero error code when one server is down but all
+        # the others are up, and a partial update was successful!
+        apt-get update || true
+      EOH
+    end
+  end
+    # Update packages
+    bash "Yum security updates" do
+      flags "-ex"
+      code <<-EOH
+        # Assume we want to ignore output for the same reason as apt-get update
+        yum -y --security update || true
+      EOH
+    end
 
 else
 
