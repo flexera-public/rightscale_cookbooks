1fa8db55ae2920429de935c72bb8696f592b8831
Erik Kargard <erik@rightscale.com>
2013-04-10 17:42:26 +0000

    [rightscale] acu50929 First pass of setup security updates

--- cookbooks/rightscale/recipes/setup_security_updates.rb
+++ cookbooks/rightscale/recipes/setup_security_updates.rb
@@ -9,7 +9,45 @@ rightscale_marker :begin
 
 if "#{node[:rightscale][:security_update]}" == "Enabled"
 
-  log "  Security updates enabled."
+  log "  Enabling security updates."
+  case node[:platform]
+  when "ubuntu"
+    bash "Unfreeze Ubuntu security repositories" do
+      flags "-ex"
+      code <<-EOH
+        # This is the Red teams bash script
+        # Set all security repos to latest
+        sed -i "s%ubuntu_daily/.* $(lsb_release -cs)-security%ubuntu_daily/latest $(lsb_release -cs)-security%" /etc/apt/sources.list.d/rightscale.sources.list
+
+        # Update the local package index
+        # Make sure we DON'T check the output of this, as apt-get update
+        # may return a non-zero error code when one server is down but all
+        # the others are up, and a partial update was successful!
+        apt-get update || true
+      EOH
+    end
+  when "centos", "redhat"
+    # Set all repos to latest
+    files = Dir.glob("/etc/yum.repos.d/*")
+
+    repo_regex = /\/archive\/20[0-9]{6}$/
+    latest = "/archive/latest"
+
+    files.each do |file_name|
+      text = File.read(file_name)
+      replace = text.gsub!(repo_regex, latest)
+      File.open(file_name, "w") { |file| file.puts replace }
+    end
+
+    # Update packages
+    bash "Yum security updates" do
+      flags "-ex"
+      code <<-EOH
+        # Assume we want to ignore output for the same reason as apt-get update
+        yum --security update || true
+      EOH
+    end
+  end
 
 else
 
