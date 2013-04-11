caca1f2be332ddf2b52da00a9589bde55fda3f34
Kannan Manickam <kannan@rightscale.com>
2013-04-11 15:47:23 -0700

    acu84321 - [rightscale] - added some input validation and included cloudmonitoring recipe

--- cookbooks/rightscale/recipes/setup_cloud.rb
+++ cookbooks/rightscale/recipes/setup_cloud.rb
@@ -8,16 +8,39 @@
 
 rightscale_marker :begin
 
+# This recipe will run only on the Rackspace Managed Cloud. See
+# cookbooks/rightscale/libraries/helper.rb for the "is_rackspace_managed_cloud"
+# method.
 if RightScale::Utils::Helper.is_rackspace_managed_cloud?
+  if node[:rightscale][:rackspace_username].empty? ||
+    node[:rightscale][:rackspace_api_key].empty? ||
+    node[:rightscale][:rackspace_tenant_id].empty?
+    raise "Inputs Rackspace Username, Rackspace API Key, and" +
+      " Rackspace Tenant ID are required for setting up the Rackspace Managed" +
+      " Cloud."
+  end
   log "  Setting up cloud-related functions for #{node[:cloud][:provider]}"
+
+  # TODO: Remove this code after RightImages are updated to remove the
+  # /root/.noupdate file.
+  file "/root/.noupdate" do
+    action :delete
+  end
+
+  # TODO: Remove these debug statements once development is complete
   log "  DEBUG: Rackspace Username: #{node[:rightscale][:rackspace_username]}"
   log "  DEBUG: Rackspace API Key: #{node[:rightscale][:rackspace_api_key]}"
   log "  DEBUG: Rackspace Tenant ID: #{node[:rightscale][:rackspace_tenant_id]}"
-
   log "  DEBUG: Region name: #{RightScale::Utils::Helper.get_rackspace_region}"
-  node[:driveclient] ||= {}
-  node[:cloud_monitoring] ||= {}
+
+  # Obtains the region of the Rackspace Managed Cloud. See
+  # cookbooks/rightscale/libraries/helper.rb for the "get_rackspace_region"
+  # method.
   region = RightScale::Utils::Helper.get_rackspace_region
+
+  # Prepare the attributes required for the driveclient cookbook and include
+  # the driveclient::default recipe.
+  node[:driveclient] ||= {}
   node[:driveclient][:apihostname] =
     case region
     when "ord", "dfw"
@@ -25,12 +48,28 @@ if RightScale::Utils::Helper.is_rackspace_managed_cloud?
     when "lon"
       "api.drivesrvr.co.uk"
     else
-      raise "Unable to detech Rackspace region"
+      raise "Unable to detect Rackspace region"
     end
   node[:driveclient][:username] = node[:rightscale][:rackspace_username]
   node[:driveclient][:password] = node[:rightscale][:rackspace_api_key]
   node[:driveclient][:accountid] = node[:rightscale][:rackspace_tenant_id]
   include_recipe "driveclient::default"
+
+  # Prepare the attributes required for the cloudmonitoring cookbook and
+  # include the cloudmonitoring::default recipe.
+  node[:cloud_monitoring] ||= {}
+  node[:cloud_monitoring][:rackspace_username] = node[:rightscale][:rackspace_username]
+  node[:cloud_monitoring][:rackspace_api_key] = node[:rightscale][:rackspace_api_key]
+  node[:cloud_monitoring][:rackspace_auth_region] =
+    case region
+    when "ord", "dfw"
+      "us"
+    when "lon"
+      "uk"
+    else
+      raise "Unable to detect Rackspace region"
+    end
+  include_recipe "cloudmonitoring::default"
 else
   log "  Cloud setup not required for this cloud #{node[:cloud][:provider]}." +
     " Skipping..."
