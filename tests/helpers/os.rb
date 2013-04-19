a21ba2121798f8e1afad434021c5048778ca922b
Erik Kargard <erik@rightscale.com>
2013-04-19 17:45:23 +0000

    acu50929 [all] Add os.rb helper

---
+++ tests/helpers/os.rb
@@ -0,0 +1,21 @@
+# Include helper objects and methods.
+require_helper "errors"
+
+# Returns the operating system type for an operatinal server.
+#
+# @param server [Server] the server to get operating system type.
+#
+# @raise [FailedProbeCommandError] if the get operating system probe fails.
+#
+# @return [String] Operating system type: "CentOS", "ubuntu", "RHEL", etc. 
+#
+def get_operating_system(server)
+  probe(server, "lsb_release -si") do |response, status|
+    raise FailedProbeCommandError, "System call to get OS failed" unless 
+      status == 0
+    os = response.chomp
+    true
+  end
+  puts "Found OS type: #{os}"
+  os
+end
