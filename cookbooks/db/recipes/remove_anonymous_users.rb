c16839a02b46edaf08c5ea27d2369da625e09333
mrusan <maksim.rusan@rightscale.com>
2013-04-30 15:35:36 +0300

    acu91718 - [db] copyright header update

--- cookbooks/db/recipes/remove_anonymous_users.rb
+++ cookbooks/db/recipes/remove_anonymous_users.rb
@@ -1,9 +1,10 @@
 #
 # Cookbook Name:: db
 #
-# Copyright RightScale, Inc. All rights reserved.  All access and use subject to the
-# RightScale Terms of Service available at http://www.rightscale.com/terms.php and,
-# if applicable, other agreements such as a RightScale Master Subscription Agreement.
+# Copyright RightScale, Inc. All rights reserved.
+# All access and use subject to the RightScale Terms of Service available at
+# http://www.rightscale.com/terms.php and, if applicable, other agreements
+# such as a RightScale Master Subscription Agreement.
 
 # Anonymous users are created by default by MySQl to allow users access the database
 # without using a username and password.
