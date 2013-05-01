819af8015aaec3cbc0f945151dee16f73e07041f
Lopaka Delp <lopaka@rightscale.com>
2013-04-30 18:23:22 -0700

    acu77737 - [sys] sys/attributes/sys_reconverge.rb - Add descriptive comments.

--- cookbooks/sys/attributes/sys_reconverge.rb
+++ cookbooks/sys/attributes/sys_reconverge.rb
@@ -6,7 +6,8 @@
 # http://www.rightscale.com/terms.php and, if applicable, other agreements
 # such as a RightScale Master Subscription Agreement.
 
-# Required attributes
-
-default[:sys][:reconverge][:splay] = "10"
+# Defines the interval in minutes to run recipe(s) in reconverge list.
 default[:sys][:reconverge][:interval] = "15"
+# Defines the plus/minus offset of reconverge interval to avoid all systems
+# from reconverging at the same time.
+default[:sys][:reconverge][:splay] = "10"
