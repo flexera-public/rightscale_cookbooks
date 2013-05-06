fb6df76140481d8a0751f63404edcf3673527a6b
Kannan Manickam <kannan@rightscale.com>
2013-05-06 13:39:50 +0530

    acu91715 - modified rightscale_marker on missing recipes

--- cookbooks/memcached/recipes/do_restart.rb
+++ cookbooks/memcached/recipes/do_restart.rb
@@ -6,7 +6,7 @@
 # http://www.rightscale.com/terms.php and, if applicable, other agreements
 # such as a RightScale Master Subscription Agreement.
 
-rightscale_marker :begin
+rightscale_marker
 
 log "  memcached: restarting"
 
@@ -14,5 +14,3 @@ log "  memcached: restarting"
 service "memcached" do
   action :restart
 end
-
-rightscale_marker :end
