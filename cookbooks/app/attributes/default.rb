0dc4ea6fdffb055a10d3c03de7b2fb197050e76c
Kannan Manickam <kannan@rightscale.com>
2013-05-07 11:15:59 +0530

    acu93020 - [app*] - added default input values in attributes file

--- cookbooks/app/attributes/default.rb
+++ cookbooks/app/attributes/default.rb
@@ -9,13 +9,17 @@
 # Set a default provider for app to connect with lb cookbook attach/detach
 # for application servers without their own provider.
 default[:app][:provider] = "app"
-# By default listen on port 8000
-default[:app][:port] = "8000"
 # By default listen on the first private IP
 default[:app][:ip] = node[:cloud][:private_ips][0]
 # IP addrs of loadbalancer requesting firewall ports to be opened to it
 default[:app][:lb_private_ip] = ""
 default[:app][:lb_public_ip] = ""
 
+# Recommended attributes
+
+# Application listen port
+default[:app][:port] = "8080"
 # The database schema name the app server uses
 default[:app][:database_name] = ""
+# Applicatio IP type given to the load balancer
+default[:app][:backend_ip_type] = "private"
