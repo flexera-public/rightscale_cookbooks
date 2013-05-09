8a337c3a079b0daf074e1077e495e1b959694784
Douglas Thrift <douglas.thrift@rightscale.com>
2013-05-09 23:40:48 +0000

    acu94706 - Bump master cookbooks version to 13.5.0.

--- cookbooks/app_django/metadata.rb
+++ cookbooks/app_django/metadata.rb
@@ -3,7 +3,7 @@ maintainer_email "support@rightscale.com"
 license          "Copyright RightScale, Inc. All rights reserved."
 description      "Installs the Django application server."
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "13.4.0"
+version          "13.5.0"
 
 # supports "centos", "~> 5.8", "~> 6.2"
 # supports "redhat", "~> 5.8"
