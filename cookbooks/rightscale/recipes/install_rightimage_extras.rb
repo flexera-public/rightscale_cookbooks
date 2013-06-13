b7b68327aba4b1c95fe4adcf39bd02f6d083b035
Erik Kargard <erik@rightscale.com>
2013-06-13 21:08:09 +0000

    [rightscale] acu78212 Added install_rightimage_extras recipe

---
+++ cookbooks/rightscale/recipes/install_rightimage_extras.rb
@@ -0,0 +1,15 @@
+#
+# Cookbook Name:: rightscale
+#
+# Copyright RightScale, Inc. All rights reserved.
+# All access and use subject to the RightScale Terms of Service available at
+# http://www.rightscale.com/terms.php and, if applicable, other agreements
+# such as a RightScale Master Subscription Agreement.
+
+rightscale_marker
+
+# Install all packages installed on the RightImages.  Use this recipe
+# to ensure non-RightImages have the packages our ServerTemplates
+# Require.
+
+package "rightimage-extras"
