a05e354cc8abc7d1e8a9a9361f176036a5b3460e
Erik Kargard <erik@rightscale.com>
2013-07-01 21:54:31 +0000

    acu50929 [rightscale] Added security monitoring clollectd plugin

---
+++ cookbooks/rightscale/files/ubuntu/update_monitor_collectd_plugin.rb
@@ -0,0 +1,71 @@
+#!/usr/bin/ruby 
+
+require 'rubygems'
+require 'getoptlong'
+
+def usage
+  puts("#{$0} -h <hostname> [-i <sample_interval>]")
+  puts("    -h: The hostname of the machine. When using RightLink, use the SERVER UUID.")
+  puts("    -i: The sample interval of the file check (in seconds).  Default: 20 seconds")
+  exit
+end
+
+
+opts = GetoptLong.new(
+            [ '--hostname', '-h', GetoptLong::REQUIRED_ARGUMENT ],
+            [ '--sample-interval', '-i',  GetoptLong::OPTIONAL_ARGUMENT ]
+)
+
+# default values
+hostname = nil
+sample_interval = 20
+
+# check for updates every hour
+update_check_freq = 3600
+
+# timestamp of last check
+last_update_check = 0
+
+# path to apt-check
+apt_check="/usr/lib/update-notifier/apt-check"
+
+
+opts.each do |opt, arg|
+  case opt
+    when '--hostname'
+      hostname = arg
+    when '--sample-interval'
+      sample_interval = arg.to_i
+  end
+  arg.inspect
+end
+
+usage if !hostname
+
+packages_to_update=Array.new(0,0)
+
+loop do
+  now = Time.now
+
+  if ( now.to_i - last_update_check ) > update_check_freq
+    packages_to_update=`#{apt_check} 2>&1`.split(";")
+    #packages_to_update.each { |x| puts x }
+
+    # Tag the server if updates are available.
+    if (packages_to_update[1].to_i > 0) then
+      system("rs_tag -a 'security_updates_available' > /dev/null 2>&1")
+    else
+      system("rs_tag -r 'security_updates_available' > /dev/null 2>&1")
+    end
+
+    last_update_check = now.to_i
+  end
+
+  puts "PUTVAL #{hostname}/update_check/gauge-pending_updates interval=#{sample_interval} #{now.to_i}:#{packages_to_update[0]}\n" 
+  puts "PUTVAL #{hostname}/update_check/gauge-pending_security_updates interval=#{sample_interval} #{now.to_i}:#{packages_to_update[1]}\n" 
+
+  STDOUT.flush
+  sleep sample_interval
+end
+
+
