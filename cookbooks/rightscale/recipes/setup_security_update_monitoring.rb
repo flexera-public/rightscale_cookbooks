fd8cae23949ff47acb2c62c7b82ef0e59ff153f9
Kannan Manickam <kannan@rightscale.com>
2013-07-02 14:17:57 -0700

    acu50929 - [rightscale::setup_security_update_monitoring] - added steps to install update_monitor plugin

--- cookbooks/rightscale/recipes/setup_security_update_monitoring.rb
+++ cookbooks/rightscale/recipes/setup_security_update_monitoring.rb
@@ -17,6 +17,26 @@ if node[:rightscale][:security_updates] == "enable"
     package "update-notifier-common"
     # Install custom collectd plugin
     #
+    directory "#{node[:rightscale][:collectd_lib]}/plugins" do
+      owner "root"
+      group "root"
+      recursive true
+      action :create
+    end
+
+    cookbook_file "#{node[:rightscale][:collectd_lib]}/plugins/update_monitor" do
+      source "update_monitor_collect_plugin.rb"
+      mode 0755
+    end
+
+    template "#{node[:rightscale][:collectd_plugin_dir]}/update_monitor.conf" do
+      source "update_monitor.conf.erb"
+      variables(
+        :collectd_lib => node[:rightscale][:collectd_lib],
+        :server_uuid => node[:rightscale][:instalce_uuid]
+      )
+      mode 0644
+    end
 
   when "centos", "redhat"
     log "  Install CentOS security monitoring package dependencies and plugin"
