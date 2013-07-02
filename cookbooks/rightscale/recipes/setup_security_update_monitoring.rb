03536ec27ab227127bfc418d272bee7a35d74643
Erik Kargard <erik@rightscale.com>
2013-07-02 18:48:05 +0000

    acu50929 [rightscale] Add security updates monitoring

--- cookbooks/rightscale/recipes/setup_security_update_monitoring.rb
+++ cookbooks/rightscale/recipes/setup_security_update_monitoring.rb
@@ -10,11 +10,20 @@ rightscale_marker
 
 if "#{node[:rightscale][:security_updates]}" == "enable"
 
-  log "  Security updates enabled."
-  log.warn "  Security updates are not implemented."
-
+  log "  Security updates enabled.  Setting up monitoring."
+  platform = node[:platform]
+  case platform
+  when "ubuntu"
+    log "  Install Ubuntu security monitoring package dependencies and plugin"
+    package update-notifier-common
+    # Install custom collectd plugin
+    #
+    
+  when "centos", "redhat"
+    log "  Install CentOS security monitoring package dependencies and plugin"
+    log "  ERROR/TBD/XXXX - centos implementation not complete"
+  else
+    log "  Usupportted OS: #{platform}"
 else
-
   log "  Security updates disabled.  Skipping monitoring setup!"
-
 end
