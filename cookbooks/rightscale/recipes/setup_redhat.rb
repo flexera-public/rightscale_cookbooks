21b0c5e99caef2336417ada68c824f086ffb9a1d
mrusan <maksim.rusan@rightscale.com>
2013-07-09 14:34:16 +0300

    acu94293 - [rightscale::setup_redhat] added logic to support stop-start

--- cookbooks/rightscale/recipes/setup_redhat.rb
+++ cookbooks/rightscale/recipes/setup_redhat.rb
@@ -8,23 +8,28 @@
 
 rightscale_marker
 
-raise "'rightscale/redhat/username' input must be set!" if
-  node[:rightscale][:redhat][:username].to_s.empty?
-raise "'rightscale/redhat/password' input must be set!" if
-  node[:rightscale][:redhat][:password].to_s.empty?
+ruby_block "register_redhat_system" do
+  not_if { ::File.exists?("/etc/sysconfig/rhn/systemid") }
+  block do
+    usr = node[:rightscale][:redhat][:username]
+    pwd = node[:rightscale][:redhat][:password]
+    raise "'rightscale/redhat/username' input must be set!" if usr.to_s.empty?
+    raise "'rightscale/redhat/password' input must be set!" if pwd.to_s.empty?
 
-# 'rhnreg_ks' is a utility for registering a system with the
-# RHN Satellite or Red Hat Network Classic.
-cmd = "rhnreg_ks --username=#{node[:rightscale][:redhat][:username]}"
-cmd << " --password=#{node[:rightscale][:redhat][:password]} --use-eus-channel"
+    # 'rhnreg_ks' is a utility for registering a system with the
+    # RHN Satellite or Red Hat Network Classic.
+    cmd = "rhnreg_ks --username=#{usr} --password=#{pwd}"
+    cmd << " --use-eus-channel --force --verbose"
 
-rhnreg_ks = Mixlib::ShellOut.new(cmd)
-rhnreg_ks.run_command
+    rhnreg_ks = Mixlib::ShellOut.new(cmd)
+    rhnreg_ks.run_command
 
-# During successful run 'rhnreg_ks' doesn't log any messages.
-# Logs STDOUT and STDERR only if command execution fails.
-unless rhnreg_ks.exitstatus == 0
-  log rhnreg_ks.stdout
-  log rhnreg_ks.stderr
-  rhnreg_ks.error!
+    # During successful run 'rhnreg_ks' doesn't log any messages.
+    # Logs STDOUT and STDERR only if command execution fails.
+    unless rhnreg_ks.exitstatus == 0
+      log rhnreg_ks.stdout
+      log rhnreg_ks.stderr
+      rhnreg_ks.error!
+    end
+  end
 end
