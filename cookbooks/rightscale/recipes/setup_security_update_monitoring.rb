1f6d82e520a83642bc1b737f2f9610a01c50e25c
Kannan Manickam <kannan@rightscale.com>
2013-07-16 16:40:11 -0700

    acu50929 - [rightscale::setup_security_update_monitoring] - install plugin regardless of the input since it is required for the alerts to work properly

--- cookbooks/rightscale/recipes/setup_security_update_monitoring.rb
+++ cookbooks/rightscale/recipes/setup_security_update_monitoring.rb
@@ -8,58 +8,52 @@
 
 rightscale_marker
 
-if node[:rightscale][:security_updates] == "enable"
-  log "  Security updates enabled. Setting up monitoring."
+# Load the exec plugin in the main config file
+# See cookbooks/rightscale/definitions/rightscale_enable_collectd_plugin.rb
+# for the "rightscale_enable_collectd_plugin" definition.
+rightscale_enable_collectd_plugin "exec"
 
-  # Load the exec plugin in the main config file
-  # See cookbooks/rightscale/definitions/rightscale_enable_collectd_plugin.rb
-  # for the "rightscale_enable_collectd_plugin" definition.
-  rightscale_enable_collectd_plugin "exec"
+# See cookbooks/rightscale/recipes/setup_monitoring.rb for the
+# "rightscale::setup_monitoring" recipe.
+include_recipe "rightscale::setup_monitoring"
 
-  # See cookbooks/rightscale/recipes/setup_monitoring.rb for the
-  # "rightscale::setup_monitoring" recipe.
-  include_recipe "rightscale::setup_monitoring"
-
-  # The package to be installed to obtain information about
-  # updates/security updates
-  package_to_install =
-    case node[:platform]
-    when "ubuntu"
-      "update-notifier-common"
-    else
-      "yum-security"
-    end
-
-  package package_to_install
-
-  log "  Install security monitoring package dependencies and plugin"
-  # Install custom collectd plugin
-  directory ::File.join(node[:rightscale][:collectd_lib], "plugins") do
-    recursive true
-    action :create
+# The package to be installed to obtain information about
+# updates/security updates
+package_to_install =
+  case node[:platform]
+  when "ubuntu"
+    "update-notifier-common"
+  else
+    "yum-security"
   end
 
-  cookbook_file ::File.join(
-    node[:rightscale][:collectd_lib],
-    "plugins",
-    "update_monitor"
-  ) do
-    source "update_monitor_collectd_plugin.rb"
-    mode 0755
-    notifies :restart, resources(:service => "collectd")
-  end
+package package_to_install
 
-  template ::File.join(
-    node[:rightscale][:collectd_plugin_dir],
-    "update_monitor.conf"
-  ) do
-    source "update_monitor.conf.erb"
-    variables(
-      :collectd_lib => node[:rightscale][:collectd_lib],
-      :server_uuid => node[:rightscale][:instance_uuid]
-    )
-    mode 0644
-  end
-else
-  log "  Security updates disabled. Skipping monitoring setup!"
+log "  Install security monitoring package dependencies and plugin"
+# Install custom collectd plugin
+directory ::File.join(node[:rightscale][:collectd_lib], "plugins") do
+  recursive true
+  action :create
+end
+
+cookbook_file ::File.join(
+  node[:rightscale][:collectd_lib],
+  "plugins",
+  "update_monitor"
+) do
+  source "update_monitor_collectd_plugin.rb"
+  mode 0755
+  notifies :restart, resources(:service => "collectd")
+end
+
+template ::File.join(
+  node[:rightscale][:collectd_plugin_dir],
+  "update_monitor.conf"
+) do
+  source "update_monitor.conf.erb"
+  variables(
+    :collectd_lib => node[:rightscale][:collectd_lib],
+    :server_uuid => node[:rightscale][:instance_uuid]
+  )
+  mode 0644
 end
