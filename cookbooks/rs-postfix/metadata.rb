cf09e07ab58c1534111452a51271e5297b71a17d
Lopaka Delp <lopaka@rightscale.com>
2013-07-16 15:18:07 -0700

    acu97263 - [rs-postfix] update metadata to include default recipe and an attribute having options

--- cookbooks/rs-postfix/metadata.rb
+++ cookbooks/rs-postfix/metadata.rb
@@ -4,3 +4,17 @@ license          "Copyright RightScale, Inc. All rights reserved."
 description      "Installs and configures rs-postfix"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          "13.5.0"
+
+depends "postfix"
+
+recipe "rs-postfix::default",
+  "Default recipe in wrapper cookbook of postfix. Logic is done here based on" +
+  " inputs to determine which recipe in postfix cookbook to run."
+
+attribute "postfix/mail_type",
+  :display_name => "Postfix Mail Type",
+  :description => "Is this node a client or server/master?",
+  :required => "optional",
+  :choice => ["client", "master"],
+  :default => "master",
+  :recipes => ["rs-postfix::default"]
