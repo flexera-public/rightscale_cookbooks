8fdc7a55440cf4704b8feedb40fd24aeae4be4db
Kannan Manickam <kannan@rightscale.com>
2013-07-17 11:42:32 -0700

    acu50929 - [rightscale] - fixed coding style issues

--- cookbooks/rightscale/files/ubuntu/update_monitor_collectd_plugin.rb
+++ cookbooks/rightscale/files/ubuntu/update_monitor_collectd_plugin.rb
@@ -1,69 +1,83 @@
 #!/usr/bin/ruby
+#
+# Cookbook Name:: rightscale
+#
+# Copyright RightScale, Inc. All rights reserved.
+# All access and use subject to the RightScale Terms of Service available at
+# http://www.rightscale.com/terms.php and, if applicable, other agreements
+# such as a RightScale Master Subscription Agreement.
 
-require 'rubygems'
-require 'getoptlong'
+require "rubygems"
+require "getoptlong"
 
 def usage
-  puts("#{$0} -h <hostname> [-i <sample_interval>]")
-  puts("    -h: The hostname of the machine. When using RightLink, use the SERVER UUID.")
-  puts("    -i: The sample interval of the file check (in seconds).  Default: 20 seconds")
+  puts "#{$0} -h <hostname> [-i <sample_interval>]"
+  puts "    -h: The hostname of the machine. When using RightLink," +
+    " use the SERVER UUID."
+  puts "    -i: The sample interval of the file check (in seconds)." +
+    " Default: 20 seconds"
   exit
 end
 
 
 opts = GetoptLong.new(
-            [ '--hostname', '-h', GetoptLong::REQUIRED_ARGUMENT ],
-            [ '--sample-interval', '-i',  GetoptLong::OPTIONAL_ARGUMENT ]
+  [ '--hostname', '-h', GetoptLong::REQUIRED_ARGUMENT ],
+  [ '--sample-interval', '-i',  GetoptLong::OPTIONAL_ARGUMENT ]
 )
 
-# default values
+# Default values
 hostname = nil
 sample_interval = 20
 
-# check for updates every hour
+# Check for updates every hour
 update_check_freq = 3600
 
-# timestamp of last check
+# Timestamp of last check
 last_update_check = 0
 
-# path to apt-check
-apt_check="/usr/lib/update-notifier/apt-check"
+# The path for the apt-check command
+apt_check = "/usr/lib/update-notifier/apt-check"
 
 opts.each do |opt, arg|
   case opt
-    when '--hostname'
-      hostname = arg
-    when '--sample-interval'
-      sample_interval = arg.to_i
+  when '--hostname'
+    hostname = arg
+  when '--sample-interval'
+    sample_interval = arg.to_i
   end
   arg.inspect
 end
 
-usage if !hostname
+usage unless hostname
 
-packages_to_update=Array.new(0,0)
+security_packages = 0
+regular_packages = 0
 
 loop do
-  now = Time.now
-
-  if ( now.to_i - last_update_check ) > update_check_freq
-    packages_to_update=`#{apt_check} 2>&1`.split(";")
+  now = Time.now.to_i
+  if (now - last_update_check) > update_check_freq
+    regular_packages, security_packages = `#{apt_check} 2>&1`.split(";")
 
     # Tag the server if updates are available.
-    if (packages_to_update[1].to_i > 0) then
-      system("rs_tag -a 'rs_monitoring:security_updates_available=true' > /dev/null 2>&1")
+    if security_packages.to_i > 0
+      system(
+        "rs_tag --add 'rs_monitoring:security_updates_available=true' >" +
+          " /dev/null 2>&1"
+      )
     else
-      system("rs_tag -r 'rs_monitoring:security_updates_available=true' > /dev/null 2>&1")
+      system(
+        "rs_tag --remove 'rs_monitoring:security_updates_available=true' >" +
+          " /dev/null 2>&1"
+      )
     end
-
-    last_update_check = now.to_i
+    last_update_check = now
   end
 
-  puts "PUTVAL #{hostname}/update_check/gauge-pending_updates interval=#{sample_interval} #{now.to_i}:#{packages_to_update[0]}\n"
-  puts "PUTVAL #{hostname}/update_check/gauge-pending_security_updates interval=#{sample_interval} #{now.to_i}:#{packages_to_update[1]}\n"
+  puts "PUTVAL #{hostname}/update_check/gauge-pending_updates" +
+    " interval=#{sample_interval} #{now}:#{packages_to_update[0]}\n"
+  puts "PUTVAL #{hostname}/update_check/gauge-pending_security_updates" +
+    " interval=#{sample_interval} #{now}:#{packages_to_update[1]}\n"
 
   STDOUT.flush
   sleep sample_interval
 end
-
-
