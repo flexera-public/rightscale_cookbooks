f0fb0bf06b3cae20d0167c0308df41f3afebaa36
Nitin Mohan <nitin@rightscale.com>
2013-07-31 18:05:03 +0000

    acu109891 - [db_postgres] Pass the timeout value into the tools

--- cookbooks/db_postgres/libraries/helper.rb
+++ cookbooks/db_postgres/libraries/helper.rb
@@ -37,7 +37,14 @@ module RightScale
             Chef::Log.warn("  Please contact Rightscale to upgrade your account.")
           end
           mount_point = new_resource.name
-          RightScale::Tools::Database.factory(:postgres, new_resource.user, new_resource.password, mount_point, Chef::Log)
+          RightScale::Tools::Database.factory(
+            :postgres,
+            new_resource.user,
+            new_resource.password,
+            mount_point,
+            new_resource.timeout,
+            Chef::Log
+          )
         end
 
         # Load replication information
