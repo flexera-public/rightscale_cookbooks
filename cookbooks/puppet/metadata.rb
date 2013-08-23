77c08a3ccca2f7a7b86f86e486a75c159f10f8e9
Lopaka Delp <lopaka@rightscale.com>
2013-08-23 12:03:36 -0700

    acu116916 - [puppet] change version in metadata to 13.5.0

--- cookbooks/puppet/metadata.rb
+++ cookbooks/puppet/metadata.rb
@@ -3,7 +3,7 @@ maintainer_email "support@rightscale.com"
 license          "Copyright RightScale, Inc. All rights reserved."
 description      "Installs and configures the Puppet Client and Master"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "13.4.0"
+version          "13.5.0"
 
 supports "centos"
 supports "redhat"
