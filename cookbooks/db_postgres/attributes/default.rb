49001162e410924651ec684719ecec6110dd34f2
mrusan <maksim.rusan@rightscale.com>
2013-08-26 14:34:12 +0300

    acu119257 - [db_postgres] added 'db_postgres/sync_mode' input, updated code to use it

--- cookbooks/db_postgres/attributes/default.rb
+++ cookbooks/db_postgres/attributes/default.rb
@@ -6,10 +6,8 @@
 # http://www.rightscale.com/terms.php and, if applicable, other agreements
 # such as a RightScale Master Subscription Agreement.
 
-# Recommended attributes
 default[:db_postgres][:server_usage] = "dedicated" # or "shared"
-
-# Optional attributes
+default[:db_postgres][:sync_mode] = "async"
 
 # Port on which postgres listens. Used by app servers to connect.
 default[:db_postgres][:port] = "5432"
