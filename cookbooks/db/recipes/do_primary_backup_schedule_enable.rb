6855eb5775bcdb800234cd5b5ca3e33d5047d565
Lopaka Delp <lopaka@rightscale.com>
2014-01-30 12:58:13 -0800

    acu152273 - [db::do_primary_backup_schedule_enable] replacing not_if, only_if with ruby conditionals for a nicer order and less audit entry noise.

--- cookbooks/db/recipes/do_primary_backup_schedule_enable.rb
+++ cookbooks/db/recipes/do_primary_backup_schedule_enable.rb
@@ -28,37 +28,35 @@ master_minute = node[:db][:backup][:primary][:master][:cron][:minute].to_s
 slave_hour = node[:db][:backup][:primary][:slave][:cron][:hour].to_s
 slave_minute = node[:db][:backup][:primary][:slave][:cron][:minute].to_s
 
-log "  Using default setting to skip scheduling primary backup cron job on master db server" do
-  only_if { node[:db][:this_is_master] && ( master_hour.empty? || master_minute.empty? ) }
-end
+# Setup Master schedule
+if node[:db][:this_is_master]
+  if master_hour.empty? || master_minute.empty?
+    log "  Using default setting to skip scheduling primary backup cron job on master db server"
+  else
+    log "  Setting up Master primary backup cron job to run at hour: #{master_hour} and minute #{master_minute}"
 
-log "  Setting up Master primary backup cron job to run at hour: #{master_hour} and minute #{master_minute}" do
-  only_if { node[:db][:this_is_master] && ( !master_hour.empty? && !master_minute.empty? ) }
-end
+    # See cookbooks/block_device/providers/default.rb for the "backup_schedule_enable" action.
+    block_device NICKNAME do
+      lineage snap_lineage
+      cron_backup_recipe "db::do_primary_backup"
+      cron_backup_hour master_hour
+      cron_backup_minute master_minute
+      persist false
+      action :backup_schedule_enable
+    end
+  end
+# Setup Slave schedule
+else
+  log "  Setting up Slave primary backup cron job to run at hour: #{slave_hour} and minute #{slave_minute}"
 
-# See cookbooks/block_device/providers/default.rb for the "backup_schedule_enable" action.
-block_device NICKNAME do
-  only_if { node[:db][:this_is_master] && ( !master_hour.empty? && !master_minute.empty? ) }
-  lineage snap_lineage
-  cron_backup_recipe "db::do_primary_backup"
-  cron_backup_hour master_hour
-  cron_backup_minute master_minute
-  persist false
-  action :backup_schedule_enable
-end
-
-log "  Setting up Slave primary backup cron job to run at hour: #{slave_hour} and minute #{slave_minute}" do
-  not_if { node[:db][:this_is_master] }
-end
-
-# See cookbooks/block_device/providers/default.rb for the "backup_schedule_enable" action.
-block_device NICKNAME do
-  only_if { true }
-  not_if { node[:db][:this_is_master] }
-  lineage snap_lineage
-  cron_backup_recipe "db::do_primary_backup"
-  cron_backup_hour slave_hour
-  cron_backup_minute slave_minute
-  persist false
-  action :backup_schedule_enable
+  # See cookbooks/block_device/providers/default.rb for the "backup_schedule_enable" action.
+  block_device NICKNAME do
+    only_if { true }
+    lineage snap_lineage
+    cron_backup_recipe "db::do_primary_backup"
+    cron_backup_hour slave_hour
+    cron_backup_minute slave_minute
+    persist false
+    action :backup_schedule_enable
+  end
 end
