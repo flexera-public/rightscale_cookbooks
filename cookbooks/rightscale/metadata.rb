f5f6e5bc5f85cc4fa1a8f742e295b5b0b23a8036
Douglas Thrift <douglas.thrift@rightscale.com>
2014-10-03 18:21:59 +0000

    Bump rightscale cookbook version to 13.5.5.

--- cookbooks/rightscale/metadata.rb
+++ cookbooks/rightscale/metadata.rb
@@ -5,7 +5,7 @@ license          "Copyright RightScale, Inc. All rights reserved."
 description      "Base recipes used to set up services used by the RightScale" +
                  " Cloud Management Platform."
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "13.5.4"
+version          "13.5.5"
 
 supports "centos"
 supports "redhat"
