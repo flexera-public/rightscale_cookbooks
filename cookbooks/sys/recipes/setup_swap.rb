3c0231dbabb9ffdff7fd0431a6415b62b1d70c95
Lopaka Delp <lopaka@rightscale.com>
2015-01-06 08:57:48 -0800

    cosmetic changes.

--- cookbooks/sys/recipes/setup_swap.rb
+++ cookbooks/sys/recipes/setup_swap.rb
@@ -42,8 +42,7 @@ if swap_size == 0
   end
 
   # Delete swap_file if it exists.
-  file "#{swap_file}" do
-    only_if { File.exists?(swap_file) }
+  file swap_file do
     backup false
     action :delete
   end
@@ -53,13 +52,14 @@ else
   # Check and create swap file
   fs_size_threshold_percent = 75
   swap_dir=File.dirname(swap_file)
+
   directory swap_dir do
     recursive true
     action :create
     not_if { swap_dir == '/' }
   end
 
-  # If swap file exists and it is not the size requested, deactivate, disable, and delete it to be recreated.
+  # If swap file exists and it is not the requested size then deactivate, disable, and delete it to be recreated.
   bash 'validate existing swapfile' do
     flags '-ex'
     not_if { File.exists?(swap_file) && File.stat(swap_file).size/1048576 == swap_size }
@@ -74,7 +74,7 @@ else
     device "#{swap_file}"
   end
   # Delete file if no longer in use as swap
-  file "#{swap_file}" do
+  file swap_file do
     not_if { File.open('/proc/swaps').grep(/^#{swap_file}\b/).any? }
     backup false
     action :delete
@@ -85,7 +85,7 @@ else
     block do
       (fs_total, fs_used) = `df --block-size=1M -P #{swap_dir} |tail -1| awk '{print $2":"$3}'`.chomp.split(":")
       if (((fs_used.to_f + swap_size).to_f/fs_total.to_f)*100).to_i > fs_size_threshold_percent
-        raise "  ERROR: Requested swap file size is too big! Currently #{fs_used}MB used out of #{fs_total}MB total. Cannot add #{swap_size}MB swap because it would exceed #{fs_size_threshold_percent}% of filesystem."
+        raise "ERROR: Requested swap file size is too big! Currently #{fs_used}MB used out of #{fs_total}MB total. Cannot add #{swap_size}MB swap because it would exceed #{fs_size_threshold_percent}% of filesystem."
       end
     end
     not_if { File.exists?(swap_file) }
@@ -104,7 +104,7 @@ else
   # set file as swap, and turn swap on if not already.
   bash 'activate swapfile' do
     not_if { File.open('/proc/swaps').grep(/^#{swap_file}\b/).any? }
-    flags "-ex"
+    flags '-ex'
     code <<-eof
       mkswap #{swap_file}
       swapon #{swap_file}
@@ -124,5 +124,5 @@ else
   # Add the collectd swap plugin to the set of collectd plugins if it isn't already there
   rightscale_enable_collectd_plugin 'swap'
   # Rebuild the collectd configuration file if necessary
-  include_recipe "rightscale::setup_monitoring"
+  include_recipe 'rightscale::setup_monitoring'
 end
